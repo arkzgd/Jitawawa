504ac88d7e53f94e2269085305841438a8148c7e
hui zhang <arkzgd@gmail.com>
2020-11-14 19:59:59 -0500

    Yet another repo which is going to be abandoned

---
+++ src/com/ihaveaname/java/playground/PlayCondition_PoliteThreads.java
@@ -0,0 +1,61 @@
+package com.ihaveaname.java.playground;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PlayCondition_PoliteThreads {
+  final Lock lock = new ReentrantLock();
+  final Condition cond = lock.newCondition();
+
+  final Thread thread1 =
+      new Thread(
+          () -> {
+            lock.lock();
+            System.out.println("Gonna call await()");
+            try {
+              cond.await();
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+            System.out.println("Out of await.");
+            lock.unlock();
+          },
+          "Worker-1");
+
+  final Thread thread2 =
+      new Thread(
+          () -> {
+            lock.lock();
+            System.out.println("Gonna call await()");
+            try {
+              cond.await();
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+            System.out.println("Out of await.");
+            lock.unlock();
+          },
+          "Worker-2");
+
+  public static void main(String[] args) throws InterruptedException {
+    PlayCondition_PoliteThreads ply = new PlayCondition_PoliteThreads();
+
+    ply.thread1.start();
+    ply.thread2.start();
+
+    int i = 10;
+    while (i > 0) {
+      Thread.sleep(1000);
+      System.out.println("-------------------");
+      System.out.println(ply.thread1.getName() + ": " + ply.thread1.getState());
+      System.out.println(ply.thread2.getName() + ": " + ply.thread2.getState());
+      i--;
+    }
+
+    // Release them
+    ply.lock.lock();
+    ply.cond.signalAll();
+    ply.lock.unlock();
+  }
+}
