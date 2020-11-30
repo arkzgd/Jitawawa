dd836c143fb272dc097bfd94f6c47111c5dad96f
hui zhang <arkzgd@gmail.com>
2020-11-29 20:53:41 -0500

    Yet another repo which is going to be abandoned

--- src/com/ihaveaname/java/tinyalgos/leetcode/ThreeSum.java
+++ src/com/ihaveaname/java/tinyalgos/leetcode/ThreeSum.java
@@ -4,65 +4,47 @@ import java.util.*;
 
 public class ThreeSum {
   public static List<List<Integer>> solution(Integer[] input, Integer target) {
-    Map<Integer, List<Integer>> map = arrayToMap(input);
-    List<Integer> keySet = new ArrayList(map.keySet());
-    int keySetLength = keySet.size();
+    Map<Integer, List<List<Integer>>> sumMap = partialSumMap(input);
     List<List<Integer>> result = new ArrayList<>();
 
-    List<List<Integer>> keys = new ArrayList<>();
-    for (int i = 0; i < keySetLength; i++) {
-      for (int j = 0; j < keySetLength; j++) {
-        for (int k = 0; k < keySetLength; k++) {
-          if (keySet.get(i) + keySet.get(j) + keySet.get(k) == target) {
-            List<Integer> list = new ArrayList<>(3);
-            list.add(keySet.get(i));
-            list.add(keySet.get(j));
-            list.add(keySet.get(k));
-            keys.add(list);
+    int length = input.length;
+    for (int i = 0; i < length; i++) {
+      int t = target - input[i];
+      if (sumMap.containsKey(t)) {
+        List<List<Integer>> l = sumMap.get(t);
+        for (int k = 0; k < l.size(); k++) {
+          if (!l.get(k).contains(input[i])) {
+            List<Integer> ll = new ArrayList<>(3);
+            ll.add(input[i]);
+            ll.addAll(l.get(k));
+            result.add(ll);
           }
         }
       }
     }
 
-    Set<List<Integer>> s = new HashSet<>();
-    for (int k = 0; k < keys.size(); k++) {
-      List<Integer> key = keys.get(k);
-      Integer a = key.get(0);
-      List<Integer> al = map.get(a);
-      int all = al.size();
-      Integer b = key.get(1);
-      List<Integer> bl = map.get(b);
-      int bll = bl.size();
-      Integer c = key.get(2);
-      List<Integer> cl = map.get(c);
-      int cll = cl.size();
-
-      for (int ai = 0; ai < all; ai++)
-        for (int bi = 0; bi < bll; bi++)
-          for (int ci = 0; ci < cll; ci++) {
-            Set<Integer> l = new HashSet<>();
-            l.add(al.get(ai));
-            l.add(bl.get(bi));
-            l.add(cl.get(ci));
-
-            if (l.size() == 3) s.add(new ArrayList<>(l));
-          }
-    }
-
-    result.addAll(s);
     return result;
   }
 
-  private static Map<Integer, List<Integer>> arrayToMap(Integer[] input) {
-    Map<Integer, List<Integer>> result = new HashMap<>();
+  private static Map<Integer, List<List<Integer>>> partialSumMap(Integer[] input) {
+    Map<Integer, List<List<Integer>>> result = new HashMap<>();
     int length = input.length;
 
-    for (int i = 0; i < length; i++) {
-      if (result.containsKey(input[i])) result.get(input[i]).add(i);
-      else {
-        List<Integer> list = new ArrayList<>();
-        list.add(i);
-        result.put(input[i], list);
+    for (int i = 0; i < length - 1; i++) {
+      for (int j = i + 1; j < length; j++) {
+        Integer k = input[i] + input[j];
+        List<Integer> l = new ArrayList<>(2);
+        l.add(input[i]);
+        l.add(input[j]);
+        if (result.containsKey(k)) {
+          List el = result.get(k);
+          el.add(l);
+          result.put(k, el);
+        } else {
+          List<List<Integer>> el = new ArrayList<>();
+          el.add(l);
+          result.put(k, el);
+        }
       }
     }
 
@@ -71,10 +53,11 @@ public class ThreeSum {
 
   public static void main(String[] args) {
     Integer[] array = {-1, 0, 1, 2, -1, -4};
-    List<List<Integer>> result = solution(array, 0);
 
-    for (int i = 0; i < result.size(); i++) {
-      System.out.println(array[result.get(i).get(0)] + " " + array[result.get(i).get(1)] + " " + array[result.get(i).get(2)]);
-    }
+    List<List<Integer>> result = solution(array, 0);
+    System.out.println(result);
+
+    result = solution(array, -1);
+    System.out.println(result);
   }
 }
