9e270d9c09cc5c9ad1011f45d9ea7297260715f5
hui zhang <arkzgd@gmail.com>
2020-12-06 17:39:51 -0500

    Yet another repo which is going to be abandoned

--- src/com/ihaveaname/java/leetcode/LongestNonRepeatString.java
+++ src/com/ihaveaname/java/leetcode/LongestNonRepeatString.java
@@ -1,19 +1,49 @@
 package com.ihaveaname.java.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestNonRepeatString {
-  public static int solution(String input) {
-    int length = input.length();
+  public static int solution_best(String input) {
     int[] location = new int[256];
     int result = 0;
 
-    int left = 0;
-    int right = 0;
-
-    for (; right < length; right++) {
-      int loc = input.charAt(right);
-      left = Math.max(left, location[loc]);
+    for (int left = 0, right = 0; right < input.length(); right++) {
+      left = Math.max(left, location[input.charAt(right)]);
       result = Math.max(result, right - left + 1);
-      location[loc] = right + 1;
+      location[input.charAt(right)] = right + 1;
+    }
+
+    return result;
+  }
+
+  public static int solution_classic(String input) {
+    int result = 0;
+
+    for (int left = 0, right = 0; right < input.length(); right++) {
+      for (int k = left; k < right; k++) {
+        if (input.charAt(k) == input.charAt(right)) {
+          left = k + 1;
+          break;
+        }
+      }
+      result = Math.max(result, right - left + 1);
+    }
+
+    return result;
+  }
+
+  public static int solution_smarter(String input) {
+    int result = 0;
+    Map<Character, Integer> occurrence = new HashMap<>();
+
+    for (int left = 0, right = 0; right < input.length(); right++) {
+      if (occurrence.containsKey(input.charAt(right))) {
+        left = Math.max(left, occurrence.get(input.charAt(right)));
+      }
+
+      occurrence.put(input.charAt(right), right + 1);
+      result = Math.max(result, right - left + 1);
     }
 
     return result;
@@ -23,7 +53,15 @@ public class LongestNonRepeatString {
     String[] ss = {"abcabcbb", "bbbbb", "pwwkew", "tmmzuxt"};
 
     for (int i = 0; i < ss.length; i++) {
-      System.out.println(ss[i] + " -> " + solution(ss[i]));
+      System.out.println(ss[i] + " -> " + solution_best(ss[i]));
+    }
+
+    for (int i = 0; i < ss.length; i++) {
+      System.out.println(ss[i] + " -> " + solution_classic(ss[i]));
+    }
+
+    for (int i = 0; i < ss.length; i++) {
+      System.out.println(ss[i] + " -> " + solution_smarter(ss[i]));
     }
   }
 }
