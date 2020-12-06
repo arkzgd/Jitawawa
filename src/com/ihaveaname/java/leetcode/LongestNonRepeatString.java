7ed5b9504842a2ff3077e2ae41b208ed30c9f36d
hui zhang <arkzgd@gmail.com>
2020-12-06 16:42:53 -0500

    Yet another repo which is going to be abandoned

--- src/com/ihaveaname/java/leetcode/LongestNonRepeatString.java
+++ src/com/ihaveaname/java/leetcode/LongestNonRepeatString.java
@@ -1,33 +1,26 @@
 package com.ihaveaname.java.leetcode;
 
-import java.util.Arrays;
-
 public class LongestNonRepeatString {
   public static int solution(String input) {
     int length = input.length();
-    int[] location = new int[128];
-    Arrays.fill(location, -1);
+    int[] location = new int[256];
     int result = 0;
 
     int left = 0;
     int right = 0;
 
     for (; right < length; right++) {
-      int loc = input.charAt(right) - 'a';
-      if (location[loc] == -1) {
-        location[loc] = right;
-        result = Math.max(result, right - left + 1);
-      } else {
-        left = Math.max(left, location[loc]) + 1;
-        location[loc] = right;
-      }
+      int loc = input.charAt(right);
+      left = Math.max(left, location[loc]);
+      result = Math.max(result, right - left + 1);
+      location[loc] = right + 1;
     }
 
     return result;
   }
 
   public static void main(String[] args) {
-    String[] ss = {"abcabcbb", "bbbbb", "pwwkew"};
+    String[] ss = {"abcabcbb", "bbbbb", "pwwkew", "tmmzuxt"};
 
     for (int i = 0; i < ss.length; i++) {
       System.out.println(ss[i] + " -> " + solution(ss[i]));
