fd53c89ed49590bf283675db2e512e343401b462
hui zhang <arkzgd@gmail.com>
2020-12-25 14:32:14 -0500

    Leetcode
    Two Sum - Input is Sorted.
    
    Brutual force with smarter search.

--- src/com/ihaveaname/java/leetcode/TwoSum_Sorted_Input.java
+++ src/com/ihaveaname/java/leetcode/TwoSum_Sorted_Input.java
@@ -3,25 +3,29 @@ package com.ihaveaname.java.leetcode;
 import java.util.Arrays;
 
 public class TwoSum_Sorted_Input {
+  int[] result = new int[2];
+  int i, j;
+
   public int[] twoSum(int[] numbers, int target) {
-    int numTarget;
-    for (int i = 0; i < numbers.length; i++) {
-      numTarget = target - numbers[i];
-      int j;
-      if ((j = Arrays.binarySearch(numbers, 0, i, numTarget)) > -1) {
-        int[] result = new int[2];
-        result[0] = j + 1;
-        result[1] = i + 1;
-        return result;
-      }
+    for (i = 0; i < numbers.length - 1; i++) {
+      j = Arrays.binarySearch(numbers, i + 1, numbers.length, target - numbers[i]);
+      if (j > -1) break;
     }
 
-    return null;
+    result[0] = i + 1;
+    result[1] = j + 1;
+    return result;
   }
 
   public static void main(String[] args) {
     TwoSum_Sorted_Input tsi = new TwoSum_Sorted_Input();
     int[] input = {2, 7, 11, 15};
     System.out.println(Arrays.toString(tsi.twoSum(input, 9)));
+
+    int[] input2 = {5, 25, 75};
+    System.out.println(Arrays.toString(tsi.twoSum(input2, 100)));
+
+    int[] input3 = {3, 24, 50, 79, 88, 150, 345};
+    System.out.println(Arrays.toString(tsi.twoSum(input3, 200)));
   }
 }
