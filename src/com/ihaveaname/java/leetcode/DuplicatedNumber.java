f30b2bd9a4704e99ffb116e7c4b8af92a33757d0
hui zhang <arkzgd@gmail.com>
2020-12-31 22:34:11 -0500

    Leetcode #287 Find the Duplicate Number
    A better, inspired solution using loop detect in Linked List.

--- src/com/ihaveaname/java/leetcode/DuplicatedNumber.java
+++ src/com/ihaveaname/java/leetcode/DuplicatedNumber.java
@@ -14,10 +14,12 @@ public class DuplicatedNumber {
     return s;
   }
 
+  // This only works when the duplicated number only appears ONCE
   public int findDuplicate(int[] nums) {
     return sum(nums) - sum(nums.length - 1);
   }
 
+  // Slow and typical solution
   public int findDuplicate_sort_and_search(int[] nums) {
     Arrays.sort(nums);
     for (int i = 0; i < nums.length - 1; i++) {
@@ -27,23 +29,46 @@ public class DuplicatedNumber {
     return 0;
   }
 
+  // Look for the duplicated array index, same idea as detecting loops in linked list
+  public int findDuplicate_cycle_detect(int[] nums) {
+    int slow = 0;
+    int fast = 0;
+
+    do {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    slow = 0;
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+
+    return slow;
+  }
+
   public static void main(String[] args) {
     DuplicatedNumber dn = new DuplicatedNumber();
 
     int[] nums = new int[]{1, 3, 4, 2, 2};
     System.out.println(dn.findDuplicate(nums));
+    System.out.println(dn.findDuplicate_cycle_detect(nums));
     System.out.println(dn.findDuplicate_sort_and_search(nums));
 
     nums = new int[]{3,1,3,4,2};
     System.out.println(dn.findDuplicate(nums));
+    System.out.println(dn.findDuplicate_cycle_detect(nums));
     System.out.println(dn.findDuplicate_sort_and_search(nums));
 
     nums = new int[]{1, 1};
     System.out.println(dn.findDuplicate(nums));
+    System.out.println(dn.findDuplicate_cycle_detect(nums));
     System.out.println(dn.findDuplicate_sort_and_search(nums));
 
     nums = new int[]{1, 1, 2};
     System.out.println(dn.findDuplicate(nums));
+    System.out.println(dn.findDuplicate_cycle_detect(nums));
     System.out.println(dn.findDuplicate_sort_and_search(nums));
   }
 }
