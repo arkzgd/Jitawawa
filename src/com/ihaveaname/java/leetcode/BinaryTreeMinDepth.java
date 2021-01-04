8796fe39a67cb80f12886e5c97e6b8921cafa9ab
hui zhang <arkzgd@gmail.com>
2021-01-03 20:12:29 -0500

    Leetcode #111 Min Depth of Binary Tree
    Easily recursive but slow and mem consuming.

---
+++ src/com/ihaveaname/java/leetcode/BinaryTreeMinDepth.java
@@ -0,0 +1,56 @@
+package com.ihaveaname.java.leetcode;
+
+public class BinaryTreeMinDepth {
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  private int getMinDepth(TreeNode node) {
+    if (node == null) return 0;
+
+    if (node.left == null) return getMinDepth(node.right) + 1;
+    else if (node.right == null) return getMinDepth(node.left) + 1;
+    else return Math.min(getMinDepth(node.left), getMinDepth(node.right)) + 1;
+  }
+
+  public int minDepth(TreeNode root) {
+    return getMinDepth(root);
+  }
+
+  public static void main(String[] args) {
+    BinaryTreeMinDepth btmd = new BinaryTreeMinDepth();
+
+    TreeNode root =
+        new TreeNode(
+            3,
+            new TreeNode(9, null, null),
+            new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null)));
+    System.out.println(btmd.minDepth(root));
+    assert btmd.minDepth(root) == 2;
+
+    root =
+        new TreeNode(
+            2,
+            null,
+            new TreeNode(
+                3,
+                null,
+                new TreeNode(4, null, new TreeNode(5, null, new TreeNode(6, null, null)))));
+    System.out.println(btmd.minDepth(root));
+    assert btmd.minDepth(root) == 5;
+  }
+}
