36d7456c452937572ab5951f22820076dbce8572
hui zhang <arkzgd@gmail.com>
2021-01-04 21:24:04 -0500

    Leetcode #1379 Find a Corresponding Node of a Binary Tree in a Clone of That Tree
    The correct shape, copy tree, find a node and a main function to test.

--- src/com/ihaveaname/java/leetcode/CopyOfNodeInClonedBinaryTree.java
+++ src/com/ihaveaname/java/leetcode/CopyOfNodeInClonedBinaryTree.java
@@ -6,18 +6,51 @@ public class CopyOfNodeInClonedBinaryTree {
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int x) {
+    TreeNode(int x, TreeNode left, TreeNode right) {
       val = x;
+      this.left = left;
+      this.right = right;
     }
+  }
+
+  private TreeNode copyTree(TreeNode root) {
+    if (root == null) return null;
+    TreeNode cloned = new TreeNode(root.val, root.left, root.right);
+    cloned.left = copyTree(root.left);
+    cloned.right = copyTree(root.right);
+
+    return cloned;
+  }
+
+  private TreeNode find(TreeNode root, int value) {
+    if (root == null) return null;
+
+    if (root.val == value) return root;
+    TreeNode inLeftTree = find(root.left, value);
+    return inLeftTree != null ? inLeftTree : find(root.right, value);
   }
 
   public final TreeNode getTargetCopy(
       final TreeNode original, final TreeNode cloned, final TreeNode target) {
-    if (cloned == null) return null;
-    int val = target.val;
-    if (cloned.val != val) {
-      TreeNode l = getTargetCopy(original, cloned.left, target);
-      return l != null ? l : getTargetCopy(original, cloned.right, target);
-    } else return cloned;
+    if (original == null || cloned == null) return null;
+    if (original == target) return cloned;
+
+    TreeNode inLeftTree = getTargetCopy(original.left, cloned.left, target);
+    return inLeftTree != null ? inLeftTree : getTargetCopy(original.right, cloned.right, target);
+  }
+
+  public static void main(String[] args) {
+    CopyOfNodeInClonedBinaryTree concbt = new CopyOfNodeInClonedBinaryTree();
+
+    TreeNode original =
+        new TreeNode(
+            7,
+            new TreeNode(4, null, null),
+            new TreeNode(3, new TreeNode(6, null, null), new TreeNode(19, null, null)));
+
+    TreeNode cloned = concbt.copyTree(original);
+
+    assert concbt.getTargetCopy(original, cloned, concbt.find(original, 3))
+        == concbt.find(cloned, 3);
   }
 }
