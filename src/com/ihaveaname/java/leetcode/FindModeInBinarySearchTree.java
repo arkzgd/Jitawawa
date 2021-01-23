cb5602c8b7da39a92e034d12b99988ba0bb5dc86
hui zhang <arkzgd@gmail.com>
2021-01-23 14:14:11 -0500

    Leetcode #501 Find Mode in Binary Search Tree
    Fastest 0 ms running time. But still... ugly. However, this is the best ugly I can understand.

--- src/com/ihaveaname/java/leetcode/FindModeInBinarySearchTree.java
+++ src/com/ihaveaname/java/leetcode/FindModeInBinarySearchTree.java
@@ -6,48 +6,51 @@ public class FindModeInBinarySearchTree {
 
   private void log(TreeNode pre, TreeNode current) {
     if (pre == null) System.out.print("pre: null");
-    else System.out.print("pre: " + pre.val + " @ " + pre.hashCode());
+    else System.out.print("pre: " + pre.val + "@" + pre.hashCode());
     System.out.print(" -> ");
     if (current == null) System.out.print("root: null");
-    else System.out.print("root: " + current.val + " @ " + current.hashCode());
-    System.out.println(" , count: " + count + " currMax: " + currMax);
+    else System.out.print("root: " + current.val + "@" + current.hashCode());
+    System.out.println(" while [count: " + count + " currMax: " + currMax + "]");
   }
 
-  TreeNode pre = null;
-  int count = 1;
-  int currMax = 1;
-  List<Integer> result = new ArrayList<>();
+  private TreeNode pre;
+  private int count;
+  private int currMax;
+  private List<Integer> result;
 
   private void inOrder(TreeNode root) {
     if (root == null) return;
+
     inOrder(root.left);
-    if (pre != null) {
-      if (pre.val == root.val) {
-        count++;
-      } else {
-        if (count > currMax) {
-          currMax = count;
-          result.clear();
-          result.add(pre.val);
-        } else if (count == currMax) {
-          result.add(pre.val);
-        }
-        count = 1;
+
+    if (pre == null) count = 1;
+
+    if (pre != null && pre.val == root.val) count++;
+    else if (pre != null && pre.val != root.val) {
+      if (count > currMax) {
+        currMax = count;
+        result.clear();
+        result.add(pre.val);
+      } else if (count == currMax) {
+        result.add(pre.val);
       }
+      count = 1;
     }
     pre = root;
+
     inOrder(root.right);
   }
 
   public int[] findMode(TreeNode root) {
     pre = null;
     count = 1;
-    currMax = 1;
-    result.clear();
+    currMax = Integer.MIN_VALUE;
+    result = new ArrayList<>();
 
     inOrder(root);
     if (pre != null) {
       if (count > currMax) {
+        currMax = count;
         result.clear();
         result.add(pre.val);
       } else if (count == currMax) {
@@ -64,16 +67,16 @@ public class FindModeInBinarySearchTree {
     FindModeInBinarySearchTree fmibst = new FindModeInBinarySearchTree();
 
     TreeNode tree = new TreeNode(1, null, new TreeNode(2, new TreeNode(2, null, null), null));
-    System.out.println(Arrays.toString(fmibst.findMode(tree)));
+    assert (Arrays.equals(fmibst.findMode(tree), new int[]{2}));
 
     tree = new TreeNode(1, null, new TreeNode(2, null, null));
-    System.out.println(Arrays.toString(fmibst.findMode(tree)));
+    assert (Arrays.equals(fmibst.findMode(tree), new int[]{1, 2}));
 
     tree = new TreeNode(1, new TreeNode(1, null, null), new TreeNode(2, null, null));
-    System.out.println(Arrays.toString(fmibst.findMode(tree)));
+    assert (Arrays.equals(fmibst.findMode(tree), new int[]{1}));
 
     tree = new TreeNode(2147483647, null, null);
-    System.out.println(Arrays.toString(fmibst.findMode(tree)));
+    assert (Arrays.equals(fmibst.findMode(tree), new int[]{2147483647}));
 
     tree =
         new TreeNode(
@@ -83,7 +86,7 @@ public class FindModeInBinarySearchTree {
                 new TreeNode(0, null, null),
                 new TreeNode(4, new TreeNode(2, null, null), new TreeNode(6, null, null))),
             new TreeNode(8, new TreeNode(7, null, null), new TreeNode(9, null, null)));
-    System.out.println(Arrays.toString(fmibst.findMode(tree)));
+    assert (Arrays.equals(fmibst.findMode(tree), new int[]{2, 6}));
 
     tree =
         new TreeNode(
@@ -96,6 +99,6 @@ public class FindModeInBinarySearchTree {
                     4,
                     new TreeNode(4, null, null),
                     new TreeNode(5, new TreeNode(5, null, null), new TreeNode(6, null, null)))));
-    System.out.println(Arrays.toString(fmibst.findMode(tree)));
+    assert (Arrays.equals(fmibst.findMode(tree), new int[]{3}));
   }
 }
