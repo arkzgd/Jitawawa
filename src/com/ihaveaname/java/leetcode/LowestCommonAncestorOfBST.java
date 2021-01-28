899a7fbee4d4a99a7823beff483ba9351554c444
hui zhang <arkzgd@gmail.com>
2021-01-27 22:45:21 -0500

    Leetcode #235 Lowest Common Ancestor of a Binary Search Tree
    Bug free but 9 ms running time on Leetcode.

--- src/com/ihaveaname/java/leetcode/LowestCommonAncestorOfBST.java
+++ src/com/ihaveaname/java/leetcode/LowestCommonAncestorOfBST.java
@@ -1,27 +1,34 @@
 package com.ihaveaname.java.leetcode;
 
+import java.util.ArrayList;
+
 public class LowestCommonAncestorOfBST {
-  private boolean reachable(TreeNode s, TreeNode t) {
+  private ArrayList<TreeNode> reachable(TreeNode s, TreeNode t) {
+    ArrayList<TreeNode> result = new ArrayList<>();
+
     while (s != null && s.val != t.val) {
+      result.add(s);
       if (s.val > t.val) s = s.left;
       else s = s.right;
     }
 
-    if (s == null) return false;
-    else return true;
+    if (s != null) result.add(t);
+
+    return result;
   }
 
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
     if (root == null) return null;
-    if (reachable(root, p) && reachable(root, q)) return root;
-
-    TreeNode l = lowestCommonAncestor(root.left, p, q);
-    if (l != null) return l;
-
-    TreeNode r = lowestCommonAncestor(root.right, p, q);
-    if (r != null) return r;
-
-    return null;
+    ArrayList<TreeNode> plist = reachable(root, p);
+    ArrayList<TreeNode> qlist = reachable(root, q);
+    int i = 0;
+    int len = Math.min(plist.size(), qlist.size());
+    while (i < len) {
+      if (plist.get(i).val == qlist.get(i).val) i++;
+      else break;
+    }
+    if (i >= len) return plist.get(i - 1);
+    else return plist.get(i == 0 ? i : i - 1);
   }
 
   public static void main(String[] args) {
@@ -36,5 +43,11 @@ public class LowestCommonAncestorOfBST {
                 new TreeNode(4, new TreeNode(3, null, null), new TreeNode(5, null, null))),
             new TreeNode(8, new TreeNode(7, null, null), new TreeNode(9, null, null)));
     System.out.println(lcaob.lowestCommonAncestor(tree, new TreeNode(0), new TreeNode(3)).val);
+    System.out.println(lcaob.lowestCommonAncestor(tree, new TreeNode(3), new TreeNode(9)).val);
+    System.out.println(lcaob.lowestCommonAncestor(tree, new TreeNode(2), new TreeNode(6)).val);
+    System.out.println(lcaob.lowestCommonAncestor(tree, new TreeNode(8), new TreeNode(6)).val);
+    System.out.println(lcaob.lowestCommonAncestor(tree, new TreeNode(7), new TreeNode(6)).val);
+    System.out.println(lcaob.lowestCommonAncestor(tree, new TreeNode(7), new TreeNode(9)).val);
+    System.out.println(lcaob.lowestCommonAncestor(tree, new TreeNode(4), new TreeNode(5)).val);
   }
 }
