09c087c20581ecedac3cc8e38e833c90ef109088
hui zhang <arkzgd@gmail.com>
2021-01-28 19:33:24 -0500

    Leetcode #235 Lowest Common Ancestor of a Binary Search Tree
    Bug free and faster 3 ms running time on Leetcode.

--- src/com/ihaveaname/java/leetcode/LowestCommonAncestorOfBST.java
+++ src/com/ihaveaname/java/leetcode/LowestCommonAncestorOfBST.java
@@ -1,34 +1,14 @@
 package com.ihaveaname.java.leetcode;
 
-import java.util.ArrayList;
-
 public class LowestCommonAncestorOfBST {
-  private ArrayList<TreeNode> reachable(TreeNode s, TreeNode t) {
-    ArrayList<TreeNode> result = new ArrayList<>();
-
-    while (s != null && s.val != t.val) {
-      result.add(s);
-      if (s.val > t.val) s = s.left;
-      else s = s.right;
-    }
-
-    if (s != null) result.add(t);
-
-    return result;
-  }
-
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    if (root == null) return null;
-    ArrayList<TreeNode> plist = reachable(root, p);
-    ArrayList<TreeNode> qlist = reachable(root, q);
-    int i = 0;
-    int len = Math.min(plist.size(), qlist.size());
-    while (i < len) {
-      if (plist.get(i).val == qlist.get(i).val) i++;
+    while (root != null) {
+      if (root.val < p.val && root.val < q.val) root = root.right;
+      else if (root.val > p.val && root.val > q.val) root = root.left;
       else break;
     }
-    if (i >= len) return plist.get(i - 1);
-    else return plist.get(i == 0 ? i : i - 1);
+
+    return root;
   }
 
   public static void main(String[] args) {
