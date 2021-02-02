b1826cd4e86c0d80313724898953840994855932
hui zhang <arkzgd@gmail.com>
2021-02-01 22:32:32 -0500

    Leetcode #589 N-ary Tree Preorder Traversal

--- src/com/ihaveaname/java/leetcode/NaryTreePreorderTraversal.java
+++ src/com/ihaveaname/java/leetcode/NaryTreePreorderTraversal.java
@@ -3,22 +3,6 @@ package com.ihaveaname.java.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
-class Node {
-  public int val;
-  public List<Node> children;
-
-  public Node() {}
-
-  public Node(int _val) {
-    val = _val;
-  }
-
-  public Node(int _val, List<Node> _children) {
-    val = _val;
-    children = _children;
-  }
-}
-
 public class NaryTreePreorderTraversal {
   private void helper(Node node, List<Integer> result) {
     if (node != null) {
