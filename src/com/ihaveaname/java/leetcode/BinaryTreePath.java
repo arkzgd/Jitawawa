61936473202613b6a276ecf58f2f495274480cd1
hui zhang <arkzgd@gmail.com>
2021-02-04 20:33:14 -0500

    Leetcode #257 Binary Tree Path

---
+++ src/com/ihaveaname/java/leetcode/BinaryTreePath.java
@@ -0,0 +1,46 @@
+package com.ihaveaname.java.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePath {
+  private List<String> path;
+
+  public BinaryTreePath() {
+    path = new ArrayList<>();
+  }
+
+  private void helper(TreeNode root, String sofar) {
+    if (root != null) {
+      if (root.left != null) helper(root.left, sofar + root.val + "->");
+      if (root.right != null) helper(root.right, sofar + root.val + "->");
+      if (root.left == null && root.right == null) {
+        path.add(sofar + root.val);
+        return;
+      }
+    }
+  }
+
+  public List<String> binaryTreePaths(TreeNode root) {
+    helper(root, "");
+    return path;
+  }
+
+  public static void main(String[] args) {
+    BinaryTreePath btp = new BinaryTreePath();
+
+    TreeNode tree =
+        new TreeNode(
+            1, new TreeNode(2, null, new TreeNode(5, null, null)), new TreeNode(3, null, null));
+    System.out.println(btp.binaryTreePaths(tree));
+
+    btp = new BinaryTreePath();
+    tree =
+      new TreeNode(
+        1,
+        new TreeNode(2, new TreeNode(3, null, null), new TreeNode(5, null, null)),
+        new TreeNode(3, null, new TreeNode(6, null, null))
+      );
+    System.out.println(btp.binaryTreePaths(tree));
+  }
+}
