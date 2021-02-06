ded3dcf424bd660d2701f181982c1a543d29070c
hui zhang <arkzgd@gmail.com>
2021-02-06 16:13:06 -0500

    Leetcode #965 Univalued Binary Tree

---
+++ src/com/ihaveaname/java/leetcode/UnivaluedBinaryTree.java
@@ -0,0 +1,32 @@
+package com.ihaveaname.java.leetcode;
+
+public class UnivaluedBinaryTree {
+  public boolean isUnivalTree(TreeNode root) {
+    if (root != null) {
+      if (root.left != null && root.left.val != root.val
+          || root.right != null && root.right.val != root.val) return false;
+      if (!isUnivalTree(root.left)) return false;
+      if (!isUnivalTree(root.right)) return false;
+    }
+
+    return true;
+  }
+
+  public static void main(String[] args) {
+    UnivaluedBinaryTree uvbt = new UnivaluedBinaryTree();
+
+    TreeNode tree =
+        new TreeNode(
+            1,
+            new TreeNode(1, new TreeNode(1, null, null), new TreeNode(1, null, null)),
+            new TreeNode(1, null, new TreeNode(1, null, null)));
+    System.out.println(uvbt.isUnivalTree(tree));
+
+    tree =
+        new TreeNode(
+            1,
+            new TreeNode(1, new TreeNode(2, null, null), new TreeNode(1, null, null)),
+            new TreeNode(1, null, new TreeNode(1, null, null)));
+    System.out.println(uvbt.isUnivalTree(tree));
+  }
+}
