a6d48298fc18d5f5b6ab5a08d8a2b414f884c53e
hui zhang <arkzgd@gmail.com>
2021-02-13 14:41:11 -0500

    Leetcode #1305 All Elements in Two Binary Search Trees
    Not real benefit if build result while traversing.

--- src/com/ihaveaname/java/leetcode/AllElementsInTwoBST.java
+++ src/com/ihaveaname/java/leetcode/AllElementsInTwoBST.java
@@ -1,17 +1,15 @@
 package com.ihaveaname.java.leetcode;
 
-import scala.reflect.internal.Trees;
-
-import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 public class AllElementsInTwoBST {
   public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-    List<Integer> result = new ArrayList<>();
-    Stack<TreeNode> stack1 = new Stack<>();
-    Stack<TreeNode> stack2 = new Stack<>();
-    while (!stack1.empty() || !stack2.empty() || root1 != null || root2 != null) {
+    List<Integer> result = new LinkedList<>();
+    Deque<TreeNode> stack1 = new LinkedList<>();
+    Deque<TreeNode> stack2 = new LinkedList<>();
+    while (!stack1.isEmpty() || !stack2.isEmpty() || root1 != null || root2 != null) {
       while (root1 != null) {
         stack1.push(root1);
         root1 = root1.left;
@@ -22,8 +20,8 @@ public class AllElementsInTwoBST {
         root2 = root2.left;
       }
 
-      root1 = stack1.empty() ? null : stack1.peek();
-      root2 = stack2.empty() ? null : stack2.peek();
+      root1 = stack1.isEmpty() ? null : stack1.peek();
+      root2 = stack2.isEmpty() ? null : stack2.peek();
       if (root1 != null && root2 != null) {
         if (root1.val == root2.val) {
           result.add(root1.val);
