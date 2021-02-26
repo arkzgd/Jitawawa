46ea2853e9caf8399509fc6e54eab07b80074fb4
hui zhang <arkzgd@gmail.com>
2021-02-25 22:56:48 -0500

    Leetcode #951 Flip Equivalent Binary Tree
    0 ms.

--- src/com/ihaveaname/java/leetcode/FlipEquivalentBinaryTree.java
+++ src/com/ihaveaname/java/leetcode/FlipEquivalentBinaryTree.java
@@ -6,26 +6,10 @@ public class FlipEquivalentBinaryTree {
       if (root1 == null && root2 == null) return true;
       else if (root1 != null && root2 != null) {
         if (root1.val != root2.val) return false;
-        else if (root1.left == null
-            && root2.left == null
-            && root1.right == null
-            && root2.right == null) return true;
+      } else if (root1 != null && root2 == null || root1 == null && root2 != null) return false;
 
-        if (root1.left != null
-            && root1.right != null
-            && root2.left != null
-            && root2.right != null) {
-          if (root1.left.val == root2.right.val && root1.right.val == root2.left.val)
-            return flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
-          else if (root1.left.val == root2.left.val && root1.right.val == root2.right.val)
-            return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
-        } else if (root1.left != null && root2.right != null && root1.left.val == root2.right.val)
-          return flipEquiv(root1.left, root2.right);
-        else if (root1.right != null && root2.left != null && root1.right.val == root2.left.val)
-          return flipEquiv(root1.right, root2.left);
-      }
-
-      return false;
+      return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
+          || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
     }
   }
 
@@ -43,26 +27,26 @@ public class FlipEquivalentBinaryTree {
             1,
             new TreeNode(3, null, new TreeNode(6)),
             new TreeNode(2, new TreeNode(4), new TreeNode(5, new TreeNode(8), new TreeNode(7))));
-    System.out.println(solution.flipEquiv(tree1, tree2));
+    assert solution.flipEquiv(tree1, tree2);
 
     tree1 = null;
     tree2 = null;
-    System.out.println(solution.flipEquiv(tree1, tree2));
+    assert solution.flipEquiv(tree1, tree2);
 
     tree1 = null;
     tree2 = new TreeNode(1);
-    System.out.println(solution.flipEquiv(tree1, tree2));
+    assert !solution.flipEquiv(tree1, tree2);
 
     tree1 = new TreeNode(0, null, new TreeNode(1));
     tree2 = null;
-    System.out.println(solution.flipEquiv(tree1, tree2));
+    assert !solution.flipEquiv(tree1, tree2);
 
     tree1 = new TreeNode(0, null, new TreeNode(1));
     tree2 = new TreeNode(0, new TreeNode(1), null);
-    System.out.println(solution.flipEquiv(tree1, tree2));
+    assert solution.flipEquiv(tree1, tree2);
 
     tree1 = new TreeNode(0, null, new TreeNode(1));
     tree2 = new TreeNode(0, null, new TreeNode(1));
-    System.out.println(solution.flipEquiv(tree1, tree2));
+    assert solution.flipEquiv(tree1, tree2);
   }
 }
