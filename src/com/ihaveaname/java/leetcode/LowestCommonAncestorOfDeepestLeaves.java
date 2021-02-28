8bec3aa3a0368a8b2fc49d75f005aea483a2d0f0
hui zhang <arkzgd@gmail.com>
2021-02-28 10:20:33 -0500

    Leetcode #1123 Lowest Common Ancestor Of Deepest Leaves
    Refactor solution.

--- src/com/ihaveaname/java/leetcode/LowestCommonAncestorOfDeepestLeaves.java
+++ src/com/ihaveaname/java/leetcode/LowestCommonAncestorOfDeepestLeaves.java
@@ -2,44 +2,39 @@ package com.ihaveaname.java.leetcode;
 
 public class LowestCommonAncestorOfDeepestLeaves {
   class Solution {
-    private int maxLevel;
-    TreeNode ans;
+    private TreeNode ans;
+    private int deepest; // deepest leaves' level so far
+    private int longest; // highest subtree eligible (left tree height == right tree height)
 
-    private void helper(TreeNode treeRoot, TreeNode root, int level) {
+    private int helper(TreeNode root, int level) {
       if (root != null) {
-        helper(treeRoot, root.left, level + 1);
-        helper(treeRoot, root.right, level + 1);
-        if (root.left == null && root.right == null) {
-          if (level > maxLevel) {
-            ans = root;
-            maxLevel = level;
-          } else if (level == maxLevel) {
-            reachable(treeRoot, root, ans);
-          }
+        int lh = helper(root.left, level + 1);
+        int rh = helper(root.right, level + 1);
+
+        // Update things only if:
+        // 1) Found a eligible (lh == rh) subtree which is higher than currently eligible; Or,
+        // 2) If found a deeper leaf node
+        // Note: lh + level == deepest is necessary, because you don't like to update ans if
+        // the subtree is actually of non-deepest leaves
+        if (lh == rh && lh > longest && lh + level == deepest || level > deepest) {
+          longest = lh;
+          ans = root;
         }
-      }
-    }
 
-    private int reachable(TreeNode s, TreeNode p, TreeNode q) {
-      if (s != null) {
-        int left = reachable(s.left, p, q) == 1 ? 1 : 0;
-        int right = reachable(s.right, p, q) == 1 ? 1 : 0;
-        int mid = s.val == p.val && s.val == q.val ? 2 : s.val == p.val || s.val == q.val ? 1 : 0;
+        if (level > deepest) deepest = level;
 
-        if (left + right + mid == 2) {
-          ans = s;
-          return 2;
-        } else return left + right + mid;
+        return Math.max(lh, rh) + 1;
       }
 
       return 0;
     }
 
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-      maxLevel = -1;
       ans = null;
+      deepest = -1;
+      longest = -1;
+      helper(root, 0);
 
-      helper(root, root, 0);
       return ans;
     }
   }
@@ -54,12 +49,12 @@ public class LowestCommonAncestorOfDeepestLeaves {
             3,
             new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))),
             new TreeNode(1, new TreeNode(0), new TreeNode(8)));
-    System.out.println(solution.lcaDeepestLeaves(tree).val);
+    assert solution.lcaDeepestLeaves(tree).val == 2;
 
     tree = new TreeNode(1);
-    System.out.println(solution.lcaDeepestLeaves(tree).val);
+    assert solution.lcaDeepestLeaves(tree).val == 1;
 
     tree = new TreeNode(0, new TreeNode(1, null, new TreeNode(2)), new TreeNode(3));
-    System.out.println(solution.lcaDeepestLeaves(tree).val);
+    assert solution.lcaDeepestLeaves(tree).val == 2;
   }
 }
