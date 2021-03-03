a13d9328ed599fbba0ba2d38fab3237fb59d4993
hui zhang <arkzgd@gmail.com>
2021-03-03 16:38:45 -0500

    Leetcode #515 Find Largest Value In Each Tree Row
    0 ms.

---
+++ src/com/ihaveaname/java/leetcode/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,48 @@
+package com.ihaveaname.java.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindLargestValueInEachTreeRow {
+  class Solution {
+    private void helper(TreeNode root, int level, ArrayList<Integer> result) {
+      if (root != null) {
+        if (level == result.size()) result.add(root.val);
+        else if (result.get(level) < root.val) result.set(level, root.val);
+        helper(root.left, level + 1, result);
+        helper(root.right, level + 1, result);
+      }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+      ArrayList<Integer> result = new ArrayList<>();
+      helper(root, 0, result);
+      return result;
+    }
+  }
+
+  public static void main(String[] args) {
+    FindLargestValueInEachTreeRow findLargestValueInEachTreeRow =
+        new FindLargestValueInEachTreeRow();
+    Solution solution = findLargestValueInEachTreeRow.new Solution();
+
+    TreeNode tree =
+        new TreeNode(
+            1,
+            new TreeNode(3, new TreeNode(5), new TreeNode(3)),
+            new TreeNode(2, null, new TreeNode(9)));
+    System.out.println(solution.largestValues(tree));
+
+    tree = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    System.out.println(solution.largestValues(tree));
+
+    tree = new TreeNode(1);
+    System.out.println(solution.largestValues(tree));
+
+    tree = new TreeNode(1, null, new TreeNode(2));
+    System.out.println(solution.largestValues(tree));
+
+    tree = null;
+    System.out.println(solution.largestValues(tree));
+  }
+}
