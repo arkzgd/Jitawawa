85c1b9f314db67ebd5e6226db793bdff4a3b7f03
hui zhang <arkzgd@gmail.com>
2021-03-13 20:38:48 -0500

    Leetcode #1466 Reorder Routes To Make All Paths Lead To The City Zero
    42 ms. Slow than average.

--- src/com/ihaveaname/java/leetcode/ReorderRoutesToMakeAllPathsLeadToTheCityZero.java
+++ src/com/ihaveaname/java/leetcode/ReorderRoutesToMakeAllPathsLeadToTheCityZero.java
@@ -1,20 +1,43 @@
 package com.ihaveaname.java.leetcode;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class ReorderRoutesToMakeAllPathsLeadToTheCityZero {
   class Solution {
+    class Adj {
+      int number;
+      boolean forward;
+
+      Adj(int number, boolean forward) {
+        this.number = number;
+        this.forward = forward;
+      }
+    }
+
+    private void dfs(int vertex, ArrayList<Adj>[] graph, boolean[] visited) {
+      if (!visited[vertex]) {
+        visited[vertex] = true;
+        for (Adj a : graph[vertex]) {
+          if (!visited[a.number] && a.forward) count++;
+          dfs(a.number, graph, visited);
+        }
+      }
+    }
+
+    private int count;
+
     public int minReorder(int n, int[][] connections) {
-      int count = 0;
-      HashSet<Integer> cities = new HashSet<>();
-      cities.add(0);
+      count = 0;
+      ArrayList<Adj>[] graph = new ArrayList[n];
+      for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
       for (int[] connection : connections) {
         int from = connection[0];
         int to = connection[1];
-        if (!cities.contains(to)) count++;
-        cities.add(from);
-        cities.add(to);
+        graph[from].add(new Adj(to, true));
+        graph[to].add(new Adj(from, false));
       }
+      boolean[] visited = new boolean[n];
+      dfs(0, graph, visited);
 
       return count;
     }
@@ -28,10 +51,13 @@ public class ReorderRoutesToMakeAllPathsLeadToTheCityZero {
     int[][] connections = new int[][] {{0, 1}, {1, 3}, {2, 3}, {4, 0}, {4, 5}};
     System.out.println(solution.minReorder(6, connections));
 
-    connections = new int[][] {{1,0},{1,2},{3,2},{3,4}};
-    System.out.println(solution.minReorder(6, connections));
+    connections = new int[][] {{1, 0}, {1, 2}, {3, 2}, {3, 4}};
+    System.out.println(solution.minReorder(5, connections));
 
     connections = new int[][] {{1, 0}, {2, 0}};
-    System.out.println(solution.minReorder(6, connections));
+    System.out.println(solution.minReorder(3, connections));
+
+    connections = new int[][] {{1, 2}, {2, 0}};
+    System.out.println(solution.minReorder(3, connections));
   }
 }
