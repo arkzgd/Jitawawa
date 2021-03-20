71514680ab75551ea56b3d967cc932a7e1e62762
hui zhang <arkzgd@gmail.com>
2021-03-19 21:34:59 -0400

    Leetcode #199 Binary Tree Right Side View
    Recursive is better and faster.

--- src/com/ihaveaname/java/leetcode/BinaryTreeRightSideView_199.java
+++ src/com/ihaveaname/java/leetcode/BinaryTreeRightSideView_199.java
@@ -7,20 +7,18 @@ import java.util.Queue;
 
 public class BinaryTreeRightSideView_199 {
   class Solution {
+    private void dfs(TreeNode root, int level, List<Integer> result) {
+      if (root != null) {
+        if (level == result.size()) result.add(root.val);
+        dfs(root.right, level + 1, result);
+        dfs(root.left, level + 1, result);
+      }
+    }
+
     public List<Integer> rightSideView(TreeNode root) {
       List<Integer> result = new ArrayList<>();
-      Queue<TreeNode> queue = new LinkedList<>();
 
-      if (root != null) queue.offer(root);
-      while (!queue.isEmpty()) {
-        int len = queue.size();
-        for (int i = 0; i < len; i++) {
-          TreeNode n = queue.poll();
-          if (i == len - 1) result.add(n.val);
-          if (n.left != null) queue.offer(n.left);
-          if (n.right != null) queue.offer(n.right);
-        }
-      }
+      dfs(root, 0, result);
 
       return result;
     }
@@ -30,11 +28,9 @@ public class BinaryTreeRightSideView_199 {
     BinaryTreeRightSideView_199 binaryTreeRightSideView_199 = new BinaryTreeRightSideView_199();
     Solution solution = binaryTreeRightSideView_199.new Solution();
 
-    TreeNode tree = new TreeNode(
-      1,
-      new TreeNode(2, null, new TreeNode(5)),
-      new TreeNode(3, null, new TreeNode(4))
-    );
+    TreeNode tree =
+        new TreeNode(
+            1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3, null, new TreeNode(4)));
     System.out.println(solution.rightSideView(tree));
 
     tree = new TreeNode(1, null, new TreeNode(3));
