673dccf31af9c0619f468586d9485a8b58442645
hui zhang <arkzgd@gmail.com>
2021-03-21 11:54:08 -0400

    Leetcode #1530 Number Of Good Leaf Nodes Pairs
    DFS + BFS, 6 ms.
    
    It is even better if using DFS post-order traversal, then at each node, loop over the leaves retruned from left and right subtrees to check if any eligibility.

--- src/com/ihaveaname/java/leetcode/NumberOfGoodLeafNodesPairs_1530.java
+++ src/com/ihaveaname/java/leetcode/NumberOfGoodLeafNodesPairs_1530.java
@@ -7,57 +7,47 @@ public class NumberOfGoodLeafNodesPairs_1530 {
     public int result;
 
     private ArrayList<Integer> dfs(TreeNode root, final int distance) {
+      ArrayList<Integer> arrayList = new ArrayList<>();
       if (root != null) {
         if (root.left == null && root.right == null) {
           ArrayList<Integer> l = new ArrayList<>();
           l.add(0);
           return l;
-        } else {
-          ArrayList<Integer> arrayList = new ArrayList<>();
-          if (root.left != null) {
-            ArrayList<Integer> lm = dfs(root.left, distance);
-            for (int i = 0; i < lm.size(); i++) lm.set(i, lm.get(i) + 1);
-            arrayList.addAll(lm);
+        }
 
-            for (Integer e : lm) {
-              if (e < distance) {
-                result += dfs_l(root.right, distance - e);
-              }
-            }
-          }
+        if (root.left != null) {
+          ArrayList<Integer> lm = dfs(root.left, distance);
+          for (int i = 0; i < lm.size(); i++) lm.set(i, lm.get(i) + 1);
+          arrayList.addAll(lm);
+        }
 
-          if (root.right != null) {
-            ArrayList<Integer> rm = dfs(root.right, distance);
-            for (int i = 0; i < rm.size(); i++) rm.set(i, rm.get(i) + 1);
-            arrayList.addAll(rm);
+        for (Integer e : arrayList) {
+          if (distance - e > 0) result += bfs(root.right, distance - e);
+        }
 
-            for (Integer e : rm) {
-              if (e < distance) {
-                result += dfs_l(root.left, distance - e);
-              }
-            }
-          }
-
-          return arrayList;
+        if (root.right != null) {
+          ArrayList<Integer> rm = dfs(root.right, distance);
+          for (int i = 0; i < rm.size(); i++) rm.set(i, rm.get(i) + 1);
+          arrayList.addAll(rm);
         }
       }
 
-      return new ArrayList<>();
+      return arrayList;
     }
 
-    private int dfs_l(TreeNode root, int length) {
+    private int bfs(TreeNode root, int length) {
       if (root != null && length > 0) {
         if (root.left == null && root.right == null) {
           return 1;
         } else {
           int lc = 0;
           if (root.left != null) {
-            lc = dfs_l(root.left, length - 1);
+            lc = bfs(root.left, length - 1);
           }
 
           int rc = 0;
           if (root.right != null) {
-            rc = dfs_l(root.right, length - 1);
+            rc = bfs(root.right, length - 1);
           }
 
           return lc + rc;
@@ -69,7 +59,7 @@ public class NumberOfGoodLeafNodesPairs_1530 {
       result = 0;
       dfs(root, distance);
 
-      return result / 2;
+      return result;
     }
   }
 
@@ -79,26 +69,29 @@ public class NumberOfGoodLeafNodesPairs_1530 {
     Solution solution = numberOfGoodLeafNodesPairs_1530.new Solution();
 
     TreeNode tree = new TreeNode(1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(3));
-    System.out.println(solution.countPairs(tree, 3));
+    assert solution.countPairs(tree, 3) == 1;
 
     tree =
         new TreeNode(
             1,
             new TreeNode(2, new TreeNode(4), new TreeNode(5)),
             new TreeNode(3, new TreeNode(6), new TreeNode(7)));
-    System.out.println(solution.countPairs(tree, 3));
+    assert solution.countPairs(tree, 3) == 2;
 
     tree =
         new TreeNode(
             7,
             new TreeNode(1, new TreeNode(6), null),
             new TreeNode(4, new TreeNode(5), new TreeNode(3, new TreeNode(2), null)));
-    System.out.println(solution.countPairs(tree, 3));
+    assert solution.countPairs(tree, 3) == 1;
 
     tree = new TreeNode(100);
-    System.out.println(solution.countPairs(tree, 1));
+    assert solution.countPairs(tree, 1) == 0;
 
     tree = new TreeNode(1, new TreeNode(1), new TreeNode(1));
-    System.out.println(solution.countPairs(tree, 2));
+    assert solution.countPairs(tree, 2) == 1;
+
+    tree = new TreeNode(1, new TreeNode(84, new TreeNode(80), new TreeNode(61)), null);
+    assert solution.countPairs(tree, 2) == 1;
   }
 }
