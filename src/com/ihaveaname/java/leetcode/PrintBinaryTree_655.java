ee6bbfb343aa792901208062b7be83f72ce32364
hui zhang <arkzgd@gmail.com>
2021-03-25 22:50:55 -0400

    Leetcode #655 Print Binary Tree
    Getting the final merged result. But not layer by layer.

--- src/com/ihaveaname/java/leetcode/PrintBinaryTree_655.java
+++ src/com/ihaveaname/java/leetcode/PrintBinaryTree_655.java
@@ -11,23 +11,45 @@ public class PrintBinaryTree_655 {
           List<String> l = new ArrayList<>();
           l.add(Integer.toString(root.val));
           return l;
-        }
-        else {
+        } else {
           List<String> ll = dfs(root.left);
           List<String> rl = dfs(root.right);
-          merge(ll, root.val, rl);
+          return merge(ll, root.val, rl);
         }
       }
 
-      return new ArrayList<>();
+      List<String> l = new ArrayList<>();
+      l.add("");
+      return l;
     }
 
-    private void merge(List<String> ll, int val, List<String> rl) {}
+    private List<String> merge(List<String> ll, int val, List<String> rl) {
+      if (ll.size() < rl.size()) {
+        int count = (rl.size() - ll.size()) / 2;
+        List<String> nll = new ArrayList<>();
+        for (int i = 0; i < count; i++) nll.add("");
+        nll.addAll(ll);
+        for (int i = 0; i < count; i++) nll.add("");
+        ll = nll;
+      } else if (ll.size() > rl.size()) {
+        int count = (ll.size() - rl.size()) / 2;
+        List<String> nrl = new ArrayList<>();
+        for (int i = 0; i < count; i++) nrl.add("");
+        nrl.addAll(rl);
+        for (int i = 0; i < count; i++) nrl.add("");
+        rl = nrl;
+      }
+
+      ll.add(Integer.toString(val));
+      ll.addAll(rl);
+
+      return ll;
+    }
 
     public List<List<String>> printTree(TreeNode root) {
       List<List<String>> result = new ArrayList<>();
 
-      dfs(root);
+      System.out.println(dfs(root));
 
       return result;
     }
@@ -38,6 +60,14 @@ public class PrintBinaryTree_655 {
     Solution solution = printBinaryTree_655.new Solution();
 
     TreeNode tree = new TreeNode(1, new TreeNode(2), null);
+    System.out.println(solution.printTree(tree));
+
+    tree = new TreeNode(1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(3));
+    System.out.println(solution.printTree(tree));
+
+    tree =
+        new TreeNode(
+            1, new TreeNode(2, new TreeNode(3, new TreeNode(4), null), null), new TreeNode(5));
     System.out.println(solution.printTree(tree));
   }
 }
