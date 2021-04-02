d02607cd74597d2b0997501b14984b91f24422da
hui zhang <arkzgd@gmail.com>
2021-04-01 22:34:00 -0400

    Leetcode #652 Find Duplicated Subtrees
    11 ms. No significant improvement.
    
    Also attached a 2 ms solution from Leetcode, and it implements hashCode by itself.

--- src/com/ihaveaname/java/leetcode/FindDuplicatedSubtrees_652.java
+++ src/com/ihaveaname/java/leetcode/FindDuplicatedSubtrees_652.java
@@ -3,9 +3,75 @@ package com.ihaveaname.java.leetcode;
 import java.util.*;
 
 public class FindDuplicatedSubtrees_652 {
+  class RefSolutionDoingHashByItself {
+    int idCnt = 1;
+    Map<Signature, Integer> sigMap = new HashMap<>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+      // get hash for every subtree, and store in a map
+      Map<Signature, Integer> map = new HashMap<>(); // hashValueId -> frequency
+      List<TreeNode> res = new ArrayList<>();
+
+      if (root == null) return res;
+      getId(root, map, res);
+
+      return res;
+    }
+
+    private int getId(TreeNode curNode, Map<Signature, Integer> map, List<TreeNode> res) {
+      if (curNode == null) {
+        return 0; // give null a ID 0
+      }
+
+      int leftSubTreeId = getId(curNode.left, map, res);
+      int rightSubTreeId = getId(curNode.right, map, res);
+
+      Signature signature = new Signature(curNode.val, leftSubTreeId, rightSubTreeId);
+
+      // hash is hard to work because the hash function parameters collide with values in treeNode
+      // change to ID so it is uniq
+      // long curHash = leftSubTreeHash * 887 + rightSubTreeHash * 997 + curNode.val + 7919;
+      if (map.containsKey(signature)) {
+        if (map.get(signature) < 2) {
+          res.add(curNode); // only record the first duplicate
+        }
+        map.put(signature, map.get(signature) + 1);
+        return sigMap.get(signature);
+      } else {
+        map.put(signature, 1);
+        idCnt++;
+        sigMap.put(signature, idCnt);
+        return idCnt;
+      }
+    }
+
+    private class Signature {
+      public int val;
+      public int leftId;
+      public int rightId;
+
+      public Signature(int val, int leftId, int rightId) {
+        this.val = val;
+        this.leftId = leftId;
+        this.rightId = rightId;
+      }
+
+      @Override
+      public boolean equals(Object sig) {
+        Signature s = (Signature) sig;
+        return this.val == s.val && this.leftId == s.leftId && this.rightId == s.rightId;
+      }
+
+      @Override
+      public int hashCode() {
+        return leftId * 13 + rightId * 17 + val + 31;
+      }
+    }
+  }
+
   class Solution {
-    private Set<String> subtrees;
-    private Map<String, TreeNode> result;
+    private HashMap<String, Integer> subtrees;
+    private List<TreeNode> result;
 
     private String helper(TreeNode root) {
       if (root != null) {
@@ -17,9 +83,12 @@ public class FindDuplicatedSubtrees_652 {
         stringBuffer.append(helper(root.right));
 
         String s = stringBuffer.toString();
-        if (subtrees.contains(s)) {
-          if (!result.containsKey(s)) result.put(s, root);
-        } else subtrees.add(s);
+        if (subtrees.containsKey(s)) {
+          if (subtrees.get(s) == 1) result.add(root);
+          subtrees.put(s, subtrees.get(s) + 1);
+        } else {
+          subtrees.put(s, 1);
+        }
 
         return s;
       }
@@ -28,13 +97,11 @@ public class FindDuplicatedSubtrees_652 {
     }
 
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-      subtrees = new HashSet<>();
-      result = new HashMap<>();
+      subtrees = new HashMap<>();
+      result = new LinkedList<>();
       helper(root);
 
-      List<TreeNode> l = new ArrayList<>();
-      l.addAll(result.values());
-      return l;
+      return result;
     }
   }
 
