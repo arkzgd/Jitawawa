4f708941bcbf982180156a70473145b85dc71038
hui zhang <arkzgd@gmail.com>
2021-04-04 12:20:19 -0400

    Leetcode #1609 Even Odd Tree
    13 ms.

---
+++ src/com/ihaveaname/java/leetcode/EvenOddTree_1609.java
@@ -0,0 +1,79 @@
+package com.ihaveaname.java.leetcode;
+
+import java.util.ArrayList;
+
+public class EvenOddTree_1609 {
+  class Solution {
+    private boolean helper(TreeNode root, int level, ArrayList<Integer> ledger) {
+      if (root != null) {
+        if (level == ledger.size()) {
+          ledger.add(-1);
+        }
+
+        int val = ledger.get(level);
+        if (level % 2 == 0) {
+          if (root.val % 2 == 0) {
+            return false;
+          }
+
+          if (val != -1 && root.val <= val) {
+            return false;
+          }
+        } else {
+          if (root.val % 2 == 1) {
+            return false;
+          }
+
+          if (val != -1 && root.val >= val) {
+            return false;
+          }
+        }
+        ledger.set(level, root.val);
+
+        if (!helper(root.left, level + 1, ledger)) {
+          return false;
+        }
+        if (!helper(root.right, level + 1, ledger)) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    public boolean isEvenOddTree(TreeNode root) {
+      ArrayList<Integer> ledger = new ArrayList<>();
+      return helper(root, 0, ledger);
+    }
+  }
+
+  public static void main(String[] args) {
+    EvenOddTree_1609 evenOddTree_1609 = new EvenOddTree_1609();
+    Solution solution = evenOddTree_1609.new Solution();
+
+    TreeNode tree =
+        new TreeNode(
+            1,
+            new TreeNode(10, new TreeNode(3, new TreeNode(12), new TreeNode(8)), null),
+            new TreeNode(
+                4, new TreeNode(7, new TreeNode(6), null), new TreeNode(9, null, new TreeNode(2))));
+    System.out.println(solution.isEvenOddTree(tree));
+
+    tree =
+        new TreeNode(
+            5,
+            new TreeNode(4, new TreeNode(3), new TreeNode(3)),
+            new TreeNode(2, new TreeNode(7), null));
+    System.out.println(solution.isEvenOddTree(tree));
+
+    tree =
+        new TreeNode(
+            5,
+            new TreeNode(9, new TreeNode(3), new TreeNode(5)),
+            new TreeNode(1, new TreeNode(7), null));
+    System.out.println(solution.isEvenOddTree(tree));
+
+    tree = new TreeNode(1);
+    System.out.println(solution.isEvenOddTree(tree));
+  }
+}
