02199efe79d4538048bfc8808c09a061619d1d82
hui zhang <arkzgd@gmail.com>
2021-04-05 20:52:11 -0400

    Leetcode #958 Check Completeness of Binary Tree
    Trying to do with FP.

--- src/com/ihaveaname/scala/leetcode/CheckCompletenessOfABinaryTree_958.scala
+++ src/com/ihaveaname/scala/leetcode/CheckCompletenessOfABinaryTree_958.scala
@@ -2,26 +2,31 @@ package com.ihaveaname.scala.leetcode
 
 import com.ihaveaname.java.leetcode.TreeNode
 
+import scala.collection.mutable.Queue
+
 object CheckCompletenessOfABinaryTree_958 extends App {
   object Solution {
-    def isCompleteTree(root: TreeNode): Boolean = ???
+
+    def isCompleteTree(root: TreeNode): Boolean = {
+      ???
+    }
   }
 
-  val tree1 =
-    new TreeNode(
-      1,
-      new TreeNode(2, new TreeNode(4), new TreeNode(5)),
-      new TreeNode(3, new TreeNode(6), null)
-    )
-  println(s"${Solution.isCompleteTree(tree1)}")
-
-  val tree2 =
-    new TreeNode(
-      1,
-      new TreeNode(2, new TreeNode(4), new TreeNode(5)),
-      new TreeNode(3, null, new TreeNode(6))
-    )
-  println(s"${Solution.isCompleteTree(tree2)}")
+//  val tree1 =
+//    new TreeNode(
+//      1,
+//      new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+//      new TreeNode(3, new TreeNode(6), null)
+//    )
+//  println(s"${Solution.isCompleteTree(tree1)}")
+//
+//  val tree2 =
+//    new TreeNode(
+//      1,
+//      new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+//      new TreeNode(3, null, new TreeNode(6))
+//    )
+//  println(s"${Solution.isCompleteTree(tree2)}")
 
   val tree3 =
     new TreeNode(
@@ -31,11 +36,11 @@ object CheckCompletenessOfABinaryTree_958 extends App {
     )
   println(s"${Solution.isCompleteTree(tree3)}")
 
-  val tree4 =
-    new TreeNode(
-      1,
-      new TreeNode(2, new TreeNode(5), null),
-      new TreeNode(3)
-    )
-  println(s"${Solution.isCompleteTree(tree4)}")
+//  val tree4 =
+//    new TreeNode(
+//      1,
+//      new TreeNode(2, new TreeNode(5), null),
+//      new TreeNode(3)
+//    )
+//  println(s"${Solution.isCompleteTree(tree4)}")
 }
