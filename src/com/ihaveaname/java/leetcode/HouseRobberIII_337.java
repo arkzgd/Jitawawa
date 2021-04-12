42b06e809a6616631410e06c2766d23aef71d1c5
hui zhang <arkzgd@gmail.com>
2021-04-11 23:30:41 -0400

    Leetcode #337 House Robber III
    3 ms. Memorization.

--- src/com/ihaveaname/java/leetcode/HouseRobberIII_337.java
+++ src/com/ihaveaname/java/leetcode/HouseRobberIII_337.java
@@ -1,14 +1,27 @@
 package com.ihaveaname.java.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HouseRobberIII_337 {
   class Solution {
+    private Map<TreeNode, Integer> trueMap;
+    private Map<TreeNode, Integer> falseMap;
+
     private int dp(TreeNode root, boolean parentRobbed) {
+      if (parentRobbed && trueMap.containsKey(root)) return trueMap.get(root);
+      if (!parentRobbed && falseMap.containsKey(root)) return falseMap.get(root);
       if (root != null) {
-        if (parentRobbed) return dp(root.left, false) + dp(root.right, false);
-        else {
-          return Math.max(
-              root.val + dp(root.left, true) + dp(root.right, true),
-              dp(root.left, false) + dp(root.right, false));
+        if (parentRobbed) {
+          int v = dp(root.left, false) + dp(root.right, false);
+          trueMap.put(root, v);
+          return v;
+        } else {
+          int trueV = dp(root.left, true) + dp(root.right, true);
+          int falseV = dp(root.left, false) + dp(root.right, false);
+          int v = Math.max(root.val + trueV, falseV);
+          falseMap.put(root, v);
+          return v;
         }
       }
 
@@ -16,6 +29,8 @@ public class HouseRobberIII_337 {
     }
 
     public int rob(TreeNode root) {
+      trueMap = new HashMap<>();
+      falseMap = new HashMap<>();
       return dp(root, false);
     }
   }
