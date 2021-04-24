e413e90024ac5b3f1480df17f90e032d407981cc
Hui ZHANG <arkzgd@gmail.com>
2021-04-24 00:17:52 -0400

    Leetcode #437 Path Sum III
    This has to be very slow.

--- src/com/ihaveaname/java/leetcode/PathSumIII_437.java
+++ src/com/ihaveaname/java/leetcode/PathSumIII_437.java
@@ -2,8 +2,28 @@ package com.ihaveaname.java.leetcode;
 
 public class PathSumIII_437 {
   class Solution {
+    public int helper(TreeNode root, int targetSum) {
+      if (root != null) {
+        if (root.val == targetSum) {
+          return 1;
+        } else if (root.left == null && root.right == null) {
+          return 0;
+        } else {
+          return helper(root.left, targetSum - root.val) + helper(root.right, targetSum - root.val);
+        }
+      }
+
+      return 0;
+    }
+
     public int pathSum(TreeNode root, int targetSum) {
-      return -1;
+      int result = 0;
+      if (root != null) {
+        result += helper(root.left, targetSum - root.val) + helper(root.right, targetSum - root.val);
+        result += pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+      }
+
+      return result;
     }
   }
 
@@ -20,5 +40,12 @@ public class PathSumIII_437 {
                 new TreeNode(2, null, new TreeNode(1))),
             new TreeNode(-3, null, new TreeNode(11)));
     System.out.println(solution.pathSum(tree, 8));
+
+    tree =
+        new TreeNode(
+            5,
+            new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2)), null),
+            new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(5), new TreeNode(1))));
+    System.out.println(solution.pathSum(tree, 22));
   }
 }
