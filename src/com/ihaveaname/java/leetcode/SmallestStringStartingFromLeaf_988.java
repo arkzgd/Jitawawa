01b26b8a83be71ba22b38cd10290035543b30a82
Hui ZHANG <arkzgd@gmail.com>
2021-04-25 23:16:11 -0400

    Leetcode #988 Smallest String Starting From Leaf
    Previous solution was wrong, because you should not compare strings until back to root; Otherwise, you are just comparing partial results.

--- src/com/ihaveaname/java/leetcode/SmallestStringStartingFromLeaf_988.java
+++ src/com/ihaveaname/java/leetcode/SmallestStringStartingFromLeaf_988.java
@@ -2,24 +2,28 @@ package com.ihaveaname.java.leetcode;
 
 public class SmallestStringStartingFromLeaf_988 {
   class Solution {
-    public String smallestFromLeaf(TreeNode root) {
-      if (root != null) {
-        StringBuilder lrsb = new StringBuilder();
-        String lr =
-            lrsb.append(smallestFromLeaf(root.left)).append((char) ('a' + root.val)).toString();
-        StringBuilder rrsb = new StringBuilder();
-        String rr =
-            rrsb.append(smallestFromLeaf(root.right)).append((char) ('a' + root.val)).toString();
-        if (java.lang.CharSequence.compare(lr, rr) >= 0) {
-          if (rr.length() > 1) return rr;
-          else return lr;
-        } else {
-          if (lr.length() > 1) return lr;
-          else return rr;
-        }
-      }
+    private String result;
 
-      return "";
+    private void helper(TreeNode root, StringBuilder sb) {
+      if (root != null) {
+        sb.insert(0, (char) (root.val + 'a'));
+        if (root.left == null && root.right == null) {
+          if (result == null || result.compareTo(sb.toString()) > 0) {
+            result = sb.toString();
+          }
+        }
+        helper(root.left, sb);
+        helper(root.right, sb);
+        sb.deleteCharAt(0);
+      }
+    }
+
+    public String smallestFromLeaf(TreeNode root) {
+      result = null;
+      StringBuilder sb = new StringBuilder();
+      helper(root, sb);
+
+      return result;
     }
   }
 
@@ -50,6 +54,14 @@ public class SmallestStringStartingFromLeaf_988 {
     System.out.println(solution.smallestFromLeaf(tree));
 
     tree = new TreeNode(0, null, new TreeNode(1));
+    System.out.println(solution.smallestFromLeaf(tree));
+
+    tree =
+        new TreeNode(
+            25,
+            new TreeNode(
+                1, new TreeNode(0, new TreeNode(1, new TreeNode(0), null), null), new TreeNode(0)),
+            null);
     System.out.println(solution.smallestFromLeaf(tree));
   }
 }
