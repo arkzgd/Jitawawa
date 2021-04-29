be5e86c01fa493218cb077f500f54bf78d5712dc
Hui ZHANG <arkzgd@gmail.com>
2021-04-28 21:51:28 -0400

    Leetcode #117 Populating Next Right Pointers In Each Node II
    Wrong solution.

---
+++ src/com/ihaveaname/java/leetcode/PopulatingNextRightPointersInEachNodeII_117.java
@@ -0,0 +1,63 @@
+package com.ihaveaname.java.leetcode;
+
+public class PopulatingNextRightPointersInEachNodeII_117 {
+  class Solution {
+    public Node connect(Node root) {
+      Node thisLevel = root;
+      Node head = root;
+      while (head != null) {
+        while (thisLevel != null) {
+          if (thisLevel.left != null) {
+            if (thisLevel.right != null) thisLevel.left.next = thisLevel.right;
+            else {
+              if (thisLevel.next != null) {
+                Node next = thisLevel.next;
+                thisLevel.left.next = next.left != null ? next.left : next.right;
+              }
+            }
+          }
+
+          if (thisLevel.right != null) {
+            if (thisLevel.next != null) {
+              Node next = thisLevel.next;
+              thisLevel.right.next = next.left != null ? next.left : next.right;
+            }
+          }
+
+          thisLevel = thisLevel.next;
+        }
+
+        while (head != null && head.left == null && head.right == null) {
+          head = head.next;
+        }
+
+        if (head != null) thisLevel = head.left != null ? head.left : head.right;
+        head = thisLevel;
+      }
+
+      return root;
+    }
+  }
+
+  public static void main(String[] args) {
+    PopulatingNextRightPointersInEachNodeII_117 populatingNextRightPointersInEachNodeII_117 =
+        new PopulatingNextRightPointersInEachNodeII_117();
+    Solution solution = populatingNextRightPointersInEachNodeII_117.new Solution();
+
+    Node tree =
+        new Node(
+            1,
+            new Node(2, new Node(4), new Node(5), null),
+            new Node(3, null, new Node(7), null),
+            null);
+    System.out.println(solution.connect(tree));
+
+    tree = new Node(1);
+    System.out.println(solution.connect(tree));
+
+    tree =
+        new Node(
+            1, new Node(2, new Node(4), null, null), new Node(3, null, new Node(7), null), null);
+    System.out.println(solution.connect(tree));
+  }
+}
