393c526827416520f3c631720897ab02287d3f98
Hui ZHANG <arkzgd@gmail.com>
2021-05-05 23:32:00 -0400

    Leetcode #1028 Recover A Tree From Preorder Traversal
    4 ms. Slow.
    
    Build tree by doing DFS.

--- src/com/ihaveaname/java/leetcode/RecoverATreeFromPreorderTraversal_1028.java
+++ src/com/ihaveaname/java/leetcode/RecoverATreeFromPreorderTraversal_1028.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class RecoverATreeFromPreorderTraversal_1028 {
   class Solution {
-    private ArrayList<int[]> store;
-
     private ArrayList<int[]> splitString(String S) {
       int i = 0;
       int start = 0;
@@ -17,6 +15,7 @@ public class RecoverATreeFromPreorderTraversal_1028 {
         start = i;
         while (i < S.length() && S.charAt(i) != '-') i++;
         pair[1] = Integer.parseInt(S.substring(start, i));
+        start = i;
         result.add(pair);
       }
 
@@ -26,12 +25,16 @@ public class RecoverATreeFromPreorderTraversal_1028 {
     private TreeNode helper(ArrayList<int[]> store, int low, int high) {
       if (low <= high) {
         TreeNode root = new TreeNode(store.get(low)[1]);
-        int i = low + 1;
-        while (i < high && store.get(i)[0] <= store.get(i + 1)[0]) i++;
-        if (i <= high) {
-          root.left = helper(store, low + 1, i);
-          root.right = helper(store, i + 1, high);
-        }
+        if (low == high) return root;
+
+        int level = store.get(low)[0] + 1;
+        int lc = low + 1;
+        while (lc <= high && store.get(lc)[0] != level) lc++;
+        int rc = lc + 1;
+        while (rc <= high && store.get(rc)[0] != level) rc++;
+        root.left = helper(store, lc, rc - 1);
+        root.right = helper(store, rc, high);
+
         return root;
       }
 
@@ -39,7 +42,7 @@ public class RecoverATreeFromPreorderTraversal_1028 {
     }
 
     public TreeNode recoverFromPreorder(String S) {
-      store = splitString(S);
+      var store = splitString(S);
       TreeNode root = helper(store, 0, store.size() - 1);
       return root;
     }
@@ -49,11 +52,16 @@ public class RecoverATreeFromPreorderTraversal_1028 {
     RecoverATreeFromPreorderTraversal_1028 recoverATreeFromPreorderTraversal_1028 =
         new RecoverATreeFromPreorderTraversal_1028();
     Solution solution = recoverATreeFromPreorderTraversal_1028.new Solution();
+    PrintBinaryTree_655 printBinaryTree_655 = new PrintBinaryTree_655();
+    PrintBinaryTree_655.Solution printer = printBinaryTree_655.new Solution();
 
     String s = "1-2--3--4-5--6--7";
-    System.out.println(solution.recoverFromPreorder(s));
+    System.out.println(printer.printTree(solution.recoverFromPreorder(s)));
 
     s = "1-401--349---90--88";
-    System.out.println(solution.recoverFromPreorder(s));
+    System.out.println(printer.printTree(solution.recoverFromPreorder(s)));
+
+    s = "1-2--3---4-5--6---7";
+    System.out.println(printer.printTree(solution.recoverFromPreorder(s)));
   }
 }
