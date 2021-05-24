d8e8d3cdc6cf86cfe892f808029ed8dbcedeea51
Hui ZHANG <arkzgd@gmail.com>
2021-05-24 12:00:07 -0400

    Leetcode #1387 Sort Integers By The Power Of Value
    Ah, slow, 85 ms.

---
+++ src/com/ihaveaname/java/leetcode/graph/SortIntegersByThePowerOfValue_1387.java
@@ -0,0 +1,76 @@
+package com.ihaveaname.java.leetcode.graph;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SortIntegersByThePowerOfValue_1387 {
+  class Solution {
+    private Map<Integer, Integer> dp = new HashMap<>();
+
+    private int power(int v) {
+      int result = 0;
+      if (dp.containsKey(v)) result = dp.get(v);
+      else {
+        int p = 0;
+        if (v > 1) {
+          if (v % 2 == 0) p = power(v / 2) + 1;
+          else p = power(3 * v + 1) + 1;
+          dp.put(v, p);
+          result = p;
+        } else {
+          dp.put(1, 0);
+        }
+      }
+
+      return result;
+    }
+
+    public int getKth(int lo, int hi, int k) {
+      Integer[] array = new Integer[hi - lo + 1];
+      for (int i = lo; i <= hi; i++) array[i - lo] = i;
+      Arrays.parallelSort(
+          array,
+          new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+              if (power(o1) > power(o2)) return 1;
+              else if (power(o1) == power(o2)) return o1 - o2;
+              else return -1;
+            }
+          });
+
+      return array[k - 1];
+    }
+  }
+
+  public static void main(String[] args) {
+    SortIntegersByThePowerOfValue_1387 sortIntegersByThePowerOfValue_1387 =
+        new SortIntegersByThePowerOfValue_1387();
+    Solution solution = sortIntegersByThePowerOfValue_1387.new Solution();
+
+    int lo = 12, hi = 15, k = 2;
+    System.out.println(solution.getKth(lo, hi, k));
+
+    lo = 1;
+    hi = 1;
+    k = 1;
+    System.out.println(solution.getKth(lo, hi, k));
+
+    lo = 7;
+    hi = 11;
+    k = 4;
+    System.out.println(solution.getKth(lo, hi, k));
+
+    lo = 10;
+    hi = 20;
+    k = 5;
+    System.out.println(solution.getKth(lo, hi, k));
+
+    lo = 1;
+    hi = 1000;
+    k = 777;
+    System.out.println(solution.getKth(lo, hi, k));
+  }
+}
