7485b94dfd40556b6005cf0f3eae129ea4c6de80
Hui ZHANG <arkzgd@gmail.com>
2021-05-25 21:35:29 -0400

    Leetcode #959 Regions Cut By Slashes
    Start, don't know how to do it.

---
+++ src/com/ihaveaname/java/leetcode/graph/RegionsCutBySlashes_959.java
@@ -0,0 +1,17 @@
+package com.ihaveaname.java.leetcode.graph;
+
+public class RegionsCutBySlashes_959 {
+  class Solution {
+    public int regionsBySlashes(String[] grid) {
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    RegionsCutBySlashes_959 regionsCutBySlashes_959 = new RegionsCutBySlashes_959();
+    Solution solution = regionsCutBySlashes_959.new Solution();
+
+    String[] grid = new String[] {" /", "/ "};
+    System.out.println(solution.regionsBySlashes(grid));
+  }
+}
