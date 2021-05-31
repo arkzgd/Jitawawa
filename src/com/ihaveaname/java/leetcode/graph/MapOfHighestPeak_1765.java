d1c0115f099828c44706680ac5605f5d862fbccb
Hui ZHANG <arkzgd@gmail.com>
2021-05-31 19:04:29 -0400

    Leetcode #1765 Map Of Highest Peak
    Time Limit Exceeded.
    
    The same thing, you should not traverse the graph again and again from each water cell.

--- src/com/ihaveaname/java/leetcode/graph/MapOfHighestPeak_1765.java
+++ src/com/ihaveaname/java/leetcode/graph/MapOfHighestPeak_1765.java
@@ -1,22 +1,29 @@
 package com.ihaveaname.java.leetcode.graph;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MapOfHighestPeak_1765 {
   class Solution {
-    int[][] result;
-    boolean[][] visited;
+    private int[][] result;
+    private boolean[][] visited;
 
     private boolean rangeCheck(int[][] isWater, int row, int col) {
       return row >= 0 && row < isWater.length && col >= 0 && col < isWater[row].length;
     }
 
-    private void setNeighbor(int[][] isWater, int[][] result, int row, int col) {
+    private void setNeighbor(
+        int[][] isWater, int[][] result, int row, int col, Queue<int[]> queue) {
       if (rangeCheck(isWater, row - 1, col) && isWater[row - 1][col] != 1) {
         result[row - 1][col] =
             result[row - 1][col] == 0
                 ? result[row][col] + 1
                 : Math.min(result[row - 1][col], result[row][col] + 1);
+        if (!visited[row - 1][col]) {
+          queue.offer(new int[] {row - 1, col});
+          visited[row - 1][col] = true;
+        }
       }
 
       if (rangeCheck(isWater, row + 1, col) && isWater[row + 1][col] != 1) {
@@ -24,6 +31,10 @@ public class MapOfHighestPeak_1765 {
             result[row + 1][col] == 0
                 ? result[row][col] + 1
                 : Math.min(result[row + 1][col], result[row][col] + 1);
+        if (!visited[row + 1][col]) {
+          queue.offer(new int[] {row + 1, col});
+          visited[row + 1][col] = true;
+        }
       }
 
       if (rangeCheck(isWater, row, col - 1) && isWater[row][col - 1] != 1) {
@@ -31,6 +42,10 @@ public class MapOfHighestPeak_1765 {
             result[row][col - 1] == 0
                 ? result[row][col] + 1
                 : Math.min(result[row][col - 1], result[row][col] + 1);
+        if (!visited[row][col - 1]) {
+          queue.offer(new int[] {row, col - 1});
+          visited[row][col - 1] = true;
+        }
       }
 
       if (rangeCheck(isWater, row, col + 1) && isWater[row][col + 1] != 1) {
@@ -38,23 +53,24 @@ public class MapOfHighestPeak_1765 {
             result[row][col + 1] == 0
                 ? result[row][col] + 1
                 : Math.min(result[row][col + 1], result[row][col] + 1);
+        if (!visited[row][col + 1]) {
+          queue.offer(new int[] {row, col + 1});
+          visited[row][col + 1] = true;
+        }
       }
     }
 
     private void bfs(int[][] isWater, int row, int col) {
-      if (!rangeCheck(isWater, row, col)) return;
-      else if (!visited[row][col]) {
-        if (isWater[row][col] == 1) {
-          result[row][col] = 0;
-        }
-
-        setNeighbor(isWater, result, row, col);
+      if (!rangeCheck(isWater, row, col) || isWater[row][col] != 1) return;
+      else {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {row, col});
         visited[row][col] = true;
-
-        bfs(isWater, row - 1, col);
-        bfs(isWater, row + 1, col);
-        bfs(isWater, row, col - 1);
-        bfs(isWater, row, col + 1);
+        result[row][col] = 0;
+        while (!queue.isEmpty()) {
+          int[] pair = queue.poll();
+          setNeighbor(isWater, result, pair[0], pair[1], queue);
+        }
       }
     }
 
@@ -83,6 +99,30 @@ public class MapOfHighestPeak_1765 {
     System.out.println(Arrays.deepToString(solution.highestPeak(isWater)));
 
     isWater = new int[][] {{0, 0, 1}, {1, 0, 0}, {0, 0, 0}};
+    System.out.println(Arrays.deepToString(solution.highestPeak(isWater)));
+
+    isWater =
+        new int[][] {
+          {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+          {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+          {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}
+        };
     System.out.println(Arrays.deepToString(solution.highestPeak(isWater)));
   }
 }
