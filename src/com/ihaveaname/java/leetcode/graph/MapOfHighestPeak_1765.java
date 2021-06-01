e131c058e736028246ecb6335aa0ee3732d638c8
Hui ZHANG <arkzgd@gmail.com>
2021-05-31 21:27:35 -0400

    Leetcode #1765 Map Of Highest Peak
    Slow, even it is MSB.

--- src/com/ihaveaname/java/leetcode/graph/MapOfHighestPeak_1765.java
+++ src/com/ihaveaname/java/leetcode/graph/MapOfHighestPeak_1765.java
@@ -6,86 +6,66 @@ import java.util.Queue;
 
 public class MapOfHighestPeak_1765 {
   class Solution {
+    private int rows;
+    private int cols;
     private int[][] result;
     private boolean[][] visited;
 
-    private boolean rangeCheck(int[][] isWater, int row, int col) {
-      return row >= 0 && row < isWater.length && col >= 0 && col < isWater[row].length;
+    private boolean rangeCheck(int row, int col) {
+      return row >= 0 && row < rows && col >= 0 && col < cols;
     }
 
     private void setNeighbor(
         int[][] isWater, int[][] result, int row, int col, Queue<int[]> queue) {
-      if (rangeCheck(isWater, row - 1, col) && isWater[row - 1][col] != 1) {
-        result[row - 1][col] =
-            result[row - 1][col] == 0
-                ? result[row][col] + 1
-                : Math.min(result[row - 1][col], result[row][col] + 1);
-        if (!visited[row - 1][col]) {
-          queue.offer(new int[] {row - 1, col});
-          visited[row - 1][col] = true;
-        }
+      if (rangeCheck(row - 1, col) && !visited[row - 1][col]) {
+        if (isWater[row - 1][col] != 1) result[row - 1][col] = result[row][col] + 1;
+        queue.offer(new int[] {row - 1, col});
+        visited[row - 1][col] = true;
       }
 
-      if (rangeCheck(isWater, row + 1, col) && isWater[row + 1][col] != 1) {
-        result[row + 1][col] =
-            result[row + 1][col] == 0
-                ? result[row][col] + 1
-                : Math.min(result[row + 1][col], result[row][col] + 1);
-        if (!visited[row + 1][col]) {
-          queue.offer(new int[] {row + 1, col});
-          visited[row + 1][col] = true;
-        }
+      if (rangeCheck(row + 1, col) && !visited[row + 1][col]) {
+        if (isWater[row + 1][col] != 1) result[row + 1][col] = result[row][col] + 1;
+        queue.offer(new int[] {row + 1, col});
+        visited[row + 1][col] = true;
       }
 
-      if (rangeCheck(isWater, row, col - 1) && isWater[row][col - 1] != 1) {
-        result[row][col - 1] =
-            result[row][col - 1] == 0
-                ? result[row][col] + 1
-                : Math.min(result[row][col - 1], result[row][col] + 1);
-        if (!visited[row][col - 1]) {
-          queue.offer(new int[] {row, col - 1});
-          visited[row][col - 1] = true;
-        }
+      if (rangeCheck(row, col - 1) && !visited[row][col - 1]) {
+        if (isWater[row][col - 1] != 1) result[row][col - 1] = result[row][col] + 1;
+        queue.offer(new int[] {row, col - 1});
+        visited[row][col - 1] = true;
       }
 
-      if (rangeCheck(isWater, row, col + 1) && isWater[row][col + 1] != 1) {
-        result[row][col + 1] =
-            result[row][col + 1] == 0
-                ? result[row][col] + 1
-                : Math.min(result[row][col + 1], result[row][col] + 1);
-        if (!visited[row][col + 1]) {
-          queue.offer(new int[] {row, col + 1});
-          visited[row][col + 1] = true;
-        }
+      if (rangeCheck(row, col + 1) && !visited[row][col + 1]) {
+        if (isWater[row][col + 1] != 1) result[row][col + 1] = result[row][col] + 1;
+        queue.offer(new int[] {row, col + 1});
+        visited[row][col + 1] = true;
       }
     }
 
-    private void bfs(int[][] isWater, int row, int col) {
-      if (!rangeCheck(isWater, row, col) || isWater[row][col] != 1) return;
-      else {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {row, col});
-        visited[row][col] = true;
-        result[row][col] = 0;
-        while (!queue.isEmpty()) {
-          int[] pair = queue.poll();
-          setNeighbor(isWater, result, pair[0], pair[1], queue);
+    private void bfs(int[][] isWater) {
+      Queue<int[]> queue = new LinkedList<>();
+      for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+          if (isWater[row][col] == 1) {
+            queue.offer(new int[] {row, col});
+            visited[row][col] = true;
+          }
         }
+      }
+
+      while (!queue.isEmpty()) {
+        int[] pair = queue.poll();
+        setNeighbor(isWater, result, pair[0], pair[1], queue);
       }
     }
 
     public int[][] highestPeak(int[][] isWater) {
-      int rows = isWater.length;
-      int cols = isWater[0].length;
+      rows = isWater.length;
+      cols = isWater[0].length;
       result = new int[rows][cols];
-      for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
-          if (isWater[row][col] == 1) {
-            visited = new boolean[rows][cols];
-            bfs(isWater, row, col);
-          }
-        }
-      }
+      visited = new boolean[rows][cols];
+
+      bfs(isWater);
 
       return result;
     }
