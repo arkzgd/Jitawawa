1008316c41d84e3785178d1b654123978b790495
Hui ZHANG <arkzgd@gmail.com>
2021-06-05 13:19:57 -0400

    Leetcode #1615 Maximal Network Rank
    39 ms. Slow like snail.

---
+++ src/com/ihaveaname/java/leetcode/graph/MaximalNetworkRank_1615.java
@@ -0,0 +1,55 @@
+package com.ihaveaname.java.leetcode.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MaximalNetworkRank_1615 {
+  class Solution {
+    private Map<Integer, Set<Integer>> toGraph(int n, int[][] roads) {
+      Map<Integer, Set<Integer>> result = new HashMap<>();
+      for (int i = 0; i < n; i++) result.put(i, new HashSet<>());
+      for (int[] pair : roads) {
+        int u = pair[0];
+        int v = pair[1];
+        result.get(u).add(v);
+        result.get(v).add(u);
+      }
+
+      return result;
+    }
+
+    public int maximalNetworkRank(int n, int[][] roads) {
+      int count = 0;
+      Map<Integer, Set<Integer>> graph = toGraph(n, roads);
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (i != j) {
+            int sum = graph.get(i).size() + graph.get(j).size();
+            if (graph.get(i).contains(j) || graph.get(j).contains(i)) sum--;
+            count = Math.max(count, sum);
+          }
+        }
+      }
+      return count;
+    }
+  }
+
+  public static void main(String[] args) {
+    MaximalNetworkRank_1615 maximalNetworkRank_1615 = new MaximalNetworkRank_1615();
+    Solution solution = maximalNetworkRank_1615.new Solution();
+
+    int n = 4;
+    int[][] roads = new int[][] {{0, 1}, {0, 3}, {1, 2}, {1, 3}};
+    System.out.println(solution.maximalNetworkRank(n, roads));
+
+    n = 5;
+    roads = new int[][] {{0, 1}, {0, 3}, {1, 2}, {1, 3}, {2, 3}, {2, 4}};
+    System.out.println(solution.maximalNetworkRank(n, roads));
+
+    n = 8;
+    roads = new int[][] {{0, 1}, {1, 2}, {2, 3}, {2, 4}, {5, 6}, {5, 7}};
+    System.out.println(solution.maximalNetworkRank(n, roads));
+  }
+}
