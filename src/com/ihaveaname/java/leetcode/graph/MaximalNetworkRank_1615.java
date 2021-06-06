5017d4f0c295ab07168b5879ddc61e5b3b89f7f5
Hui ZHANG <arkzgd@gmail.com>
2021-06-06 11:02:46 -0400

    Leetcode #1615 Maximal Network Rank
    7 ms. I think that is it.

--- src/com/ihaveaname/java/leetcode/graph/MaximalNetworkRank_1615.java
+++ src/com/ihaveaname/java/leetcode/graph/MaximalNetworkRank_1615.java
@@ -1,32 +1,37 @@
 package com.ihaveaname.java.leetcode.graph;
 
-import java.util.*;
-
 public class MaximalNetworkRank_1615 {
   class Solution {
-    private List<Set<Integer>> toGraph(int n, int[][] roads) {
-      List<Set<Integer>> result = new ArrayList<>(n);
-      for (int i = 0; i < n; i++) result.add(new HashSet<>());
-      for (int[] pair : roads) {
-        int u = pair[0];
-        int v = pair[1];
-        result.get(u).add(v);
-        result.get(v).add(u);
+    static class Statistic {
+      Statistic(int[] degrees, int[][] connected) {
+        this.degrees = degrees;
+        this.connected = connected;
       }
 
-      return result;
+      int[] degrees;
+      int[][] connected;
+    }
+
+    private Statistic toNodeDegrees(int n, int[][] roads) {
+      int[] degrees = new int[n];
+      int[][] connected = new int[n][n];
+      for (int[] road : roads) {
+        degrees[road[0]]++;
+        degrees[road[1]]++;
+        connected[road[0]][road[1]] = 1;
+        connected[road[1]][road[0]] = 1;
+      }
+
+      return new Statistic(degrees, connected);
     }
 
     public int maximalNetworkRank(int n, int[][] roads) {
       int count = 0;
-      List<Set<Integer>> graph = toGraph(n, roads);
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          if (i != j) {
-            int sum = graph.get(i).size() + graph.get(j).size();
-            if (graph.get(i).contains(j) || graph.get(j).contains(i)) sum--;
-            count = Math.max(count, sum);
-          }
+      Statistic statistic = toNodeDegrees(n, roads);
+      for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+          int sum = statistic.degrees[i] + statistic.degrees[j] - statistic.connected[i][j];
+          count = Math.max(count, sum);
         }
       }
       return count;
