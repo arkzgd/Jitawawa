82d20808a7a4d1301c78c1cb36fa7d7d462ed1b4
Hui ZHANG <arkzgd@gmail.com>
2021-06-10 22:22:12 -0400

    Leetcode #1631 Path With Minimum Effort
    Start.

---
+++ src/com/ihaveaname/java/leetcode/graph/PathWithMinimumEffort_1631.java
@@ -0,0 +1,17 @@
+package com.ihaveaname.java.leetcode.graph;
+
+public class PathWithMinimumEffort_1631 {
+  class Solution {
+    public int minimumEffortPath(int[][] heights) {
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    PathWithMinimumEffort_1631 pathWithMinimumEffort_1631 = new PathWithMinimumEffort_1631();
+    Solution solution = pathWithMinimumEffort_1631.new Solution();
+
+    int[][] heights = new int[][] {{1, 2, 2}, {3, 8, 2}, {5, 3, 5}};
+    System.out.println(solution.minimumEffortPath(heights));
+  }
+}
