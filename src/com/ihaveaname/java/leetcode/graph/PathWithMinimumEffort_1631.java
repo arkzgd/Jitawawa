e1dc949e76034e5cce5538baeed0ca435373ceda
Hui ZHANG <arkzgd@gmail.com>
2021-06-11 23:57:35 -0400

    Leetcode #1631 Path With Minimum Effort
    Placeholder.

--- src/com/ihaveaname/java/leetcode/graph/PathWithMinimumEffort_1631.java
+++ src/com/ihaveaname/java/leetcode/graph/PathWithMinimumEffort_1631.java
@@ -2,8 +2,25 @@ package com.ihaveaname.java.leetcode.graph;
 
 public class PathWithMinimumEffort_1631 {
   class Solution {
+    private int min;
+    private int[][] where = new int[][] {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    private void dfs(
+        int i, int j, int[][] heights, int rows, int cols, int soFar, int[][] visited) {
+      if (i == rows - 1 && j == cols - 1) return;
+      for (int[] move : where) {
+
+      }
+    }
+
     public int minimumEffortPath(int[][] heights) {
-      return 0;
+      min = Integer.MAX_VALUE;
+      int rows = heights.length;
+      int cols = heights[0].length;
+      int[][] visited = new int[rows][cols];
+      dfs(0, 0, heights, rows, cols, 0, visited);
+
+      return min;
     }
   }
 
