4c5c79bbc63ccf77f35f2d30eec4cfae17491641
Hui ZHANG <arkzgd@gmail.com>
2021-06-24 21:22:35 -0400

    Leetcode #1334 Find The City With The Smallest Number Of Neighbors At A Threshold Distance
    Start.

---
+++ src/com/ihaveaname/java/leetcode/graph/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334.java
@@ -0,0 +1,19 @@
+package com.ihaveaname.java.leetcode.graph;
+
+public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334 {
+    class Solution {
+        public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+            return 0;
+        }
+    }
+
+  public static void main(String[] args) {
+    FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334 findTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334 = new FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334();
+    Solution solution = findTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334.new Solution();
+
+    int n = 4;
+    int[][] edges = new int[][] {{0,1,3},{1,2,1},{1,3,4},{2,3,1}};
+    int distanceThreshold = 4;
+    System.out.println(solution.findTheCity(n, edges, distanceThreshold));
+  }
+}
