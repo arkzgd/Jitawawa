c250504c4bae274cdf21ce76bcbca10ac778eec1
Hui ZHANG <arkzgd@gmail.com>
2021-06-26 12:31:56 -0400

    Leetcode #1334 Find The City With The Smallest Number Of Neighbors At A Threshold Distance
    Wrong again. Not correctly understood the question.

--- src/com/ihaveaname/java/leetcode/graph/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334.java
+++ src/com/ihaveaname/java/leetcode/graph/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334.java
@@ -1,9 +1,14 @@
 package com.ihaveaname.java.leetcode.graph;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334 {
   class Solution {
+    private int ans;
+    private int shortest;
+
     private ArrayList<ArrayList<Integer[]>> toGraph(int n, int[][] edges) {
       ArrayList<ArrayList<Integer[]>> result = new ArrayList<>();
       for (int i = 0; i < n; i++) result.add(new ArrayList<>());
@@ -16,32 +21,39 @@ public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_133
       return result;
     }
 
-    private int dfs(
-        int s, ArrayList<ArrayList<Integer[]>> graph, int threshold, boolean[] visited) {
+    private void dfs(
+        int s,
+        ArrayList<ArrayList<Integer[]>> graph,
+        int threshold,
+        boolean[] visited,
+        Set<Integer> soFar) {
       if (!visited[s]) {
         visited[s] = true;
-        int count = 0;
+        soFar.add(s);
         for (Integer[] neighbor : graph.get(s)) {
           if (neighbor[1] <= threshold) {
-            count++;
-            dfs(neighbor[0], graph, threshold - neighbor[1], visited);
+            dfs(neighbor[0], graph, threshold - neighbor[1], visited, soFar);
           }
         }
-
-        return count + 1;
       }
-
-      return 0;
     }
 
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
       ArrayList<ArrayList<Integer[]>> graph = toGraph(n, edges);
+      ans = -1;
+      shortest = Integer.MAX_VALUE;
       for (int i = 0; i < n; i++) {
-        System.out.println(
-            " Of: " + i + " Get: " + dfs(i, graph, distanceThreshold, new boolean[n]));
+        Set<Integer> soFar = new HashSet<>();
+        dfs(i, graph, distanceThreshold, new boolean[n], soFar);
+        soFar.remove(i);
+        System.out.println(" Of: " + i + " Get: " + soFar);
+        if (soFar.size() <= shortest) {
+          shortest = soFar.size();
+          ans = i;
+        }
       }
 
-      return 0;
+      return ans;
     }
   }
 
@@ -55,6 +67,16 @@ public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_133
     int n = 4;
     int[][] edges = new int[][] {{0, 1, 3}, {1, 2, 1}, {1, 3, 4}, {2, 3, 1}};
     int distanceThreshold = 4;
+    System.out.println(solution.findTheCity(n, edges, distanceThreshold));
+
+    n = 5;
+    edges = new int[][] {{0, 1, 2}, {0, 4, 8}, {1, 2, 3}, {1, 4, 2}, {2, 3, 1}, {3, 4, 1}};
+    distanceThreshold = 2;
+    System.out.println(solution.findTheCity(n, edges, distanceThreshold));
+
+    n = 6;
+    edges = new int[][] {{0, 1, 10}, {0, 2, 1}, {2, 3, 1}, {1, 3, 1}, {1, 4, 1}, {4, 5, 10}};
+    distanceThreshold = 20;
     System.out.println(solution.findTheCity(n, edges, distanceThreshold));
   }
 }
