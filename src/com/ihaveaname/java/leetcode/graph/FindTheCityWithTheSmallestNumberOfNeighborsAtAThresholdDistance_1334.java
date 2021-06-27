03070d24303c32792b1aca7d1672f72bd83ec3fa
Hui ZHANG <arkzgd@gmail.com>
2021-06-27 11:41:14 -0400

    Leetcode #1334 Find The City With The Smallest Number Of Neighbors At A Threshold Distance
    72 ms.

--- src/com/ihaveaname/java/leetcode/graph/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334.java
+++ src/com/ihaveaname/java/leetcode/graph/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334.java
@@ -1,56 +1,79 @@
 package com.ihaveaname.java.leetcode.graph;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_1334 {
   class Solution {
     private int ans;
     private int shortest;
 
-    private ArrayList<ArrayList<Integer[]>> toGraph(int n, int[][] edges) {
-      ArrayList<ArrayList<Integer[]>> result = new ArrayList<>();
-      for (int i = 0; i < n; i++) result.add(new ArrayList<>());
+    private class Vertex {
+      List<Integer[]> adj = new ArrayList<>();
+      boolean known;
+      int dist;
+      Vertex path;
+    }
+
+    private ArrayList<Vertex> toGraph(int n, int[][] edges) {
+      ArrayList<Vertex> result = new ArrayList<>();
+      for (int i = 0; i < n; i++) result.add(new Vertex());
 
       for (int[] e : edges) {
-        result.get(e[0]).add(new Integer[] {e[1], e[2]});
-        result.get(e[1]).add(new Integer[] {e[0], e[2]});
+        result.get(e[0]).adj.add(new Integer[] {e[1], e[2]});
+        result.get(e[1]).adj.add(new Integer[] {e[0], e[2]});
       }
 
       return result;
     }
 
-    private int dfs(
-        int s,
-        ArrayList<ArrayList<Integer[]>> graph,
-        int soFar,
-        Set<Integer> saved,
-        final int distanceThreshold) {
-      if (soFar <= distanceThreshold) {
-        Set<Integer> save = new HashSet<>();
-        save.add(s);
-        for (Integer[] neighbor : graph.get(s)) {
-          dfs(neighbor[0], graph, soFar + neighbor[1], save, distanceThreshold);
-        }
-        saved.addAll(save);
-
-        return save.size() - 1;
+    private void dijkstra(Vertex s, ArrayList<Vertex> graph) {
+      for (Vertex v : graph) {
+        v.dist = Integer.MAX_VALUE;
+        v.known = false;
+        v.path = null;
       }
 
-      return 0;
+      PriorityQueue<Vertex> queue =
+          new PriorityQueue<>(
+              new Comparator<Vertex>() {
+                @Override
+                public int compare(Vertex o1, Vertex o2) {
+                  return o1.dist - o2.dist;
+                }
+              });
+      s.dist = 0;
+      queue.offer(s);
+
+      while (!queue.isEmpty()) {
+        Vertex v = queue.poll();
+        v.known = true;
+        for (Integer[] neighbor : v.adj) {
+          Vertex w = graph.get(neighbor[0]);
+          if (!w.known) {
+            if (v.dist + neighbor[1] < w.dist) {
+              w.dist = v.dist + neighbor[1];
+              w.path = v;
+              queue.offer(w);
+            }
+          }
+        }
+      }
     }
 
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-      ArrayList<ArrayList<Integer[]>> graph = toGraph(n, edges);
+      ArrayList<Vertex> graph = toGraph(n, edges);
       ans = -1;
       shortest = Integer.MAX_VALUE;
+
       for (int i = 0; i < n; i++) {
-        int r = dfs(i, graph, 0, new HashSet<>(), distanceThreshold);
-        System.out.println("i: " + i + " r: " + r);
-        if (r <= shortest) {
+        dijkstra(graph.get(i), graph);
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+          if (graph.get(j).dist > 0 && graph.get(j).dist <= distanceThreshold) count++;
+        }
+        if (count <= shortest) {
+          shortest = count;
           ans = i;
-          shortest = r;
         }
       }
 
@@ -81,7 +104,7 @@ public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance_133
     System.out.println(solution.findTheCity(n, edges, distanceThreshold));
 
     n = 6;
-    edges = new int[][] {{0,3,7},{2,4,1},{0,1,5},{2,3,10},{1,3,6},{1,2,1}};
+    edges = new int[][] {{0, 3, 7}, {2, 4, 1}, {0, 1, 5}, {2, 3, 10}, {1, 3, 6}, {1, 2, 1}};
     distanceThreshold = 417;
     System.out.println(solution.findTheCity(n, edges, distanceThreshold));
   }
