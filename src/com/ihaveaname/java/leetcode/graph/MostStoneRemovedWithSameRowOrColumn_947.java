9bb2e849713079a894662d64447f04567f440d57
Hui ZHANG <arkzgd@gmail.com>
2021-06-29 22:38:17 -0400

    Leetcode #947 Most Stone Removed With Same Row Or Column
    Start.

---
+++ src/com/ihaveaname/java/leetcode/graph/MostStoneRemovedWithSameRowOrColumn_947.java
@@ -0,0 +1,17 @@
+package com.ihaveaname.java.leetcode.graph;
+
+public class MostStoneRemovedWithSameRowOrColumn_947 {
+  class Solution {
+    public int removeStones(int[][] stones) {
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    MostStoneRemovedWithSameRowOrColumn_947 mostStoneRemovedWithSameRowOrColumn_947 =
+        new MostStoneRemovedWithSameRowOrColumn_947();
+    Solution solution = mostStoneRemovedWithSameRowOrColumn_947.new Solution();
+    int[][] stones = new int[][] {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
+    System.out.println(solution.removeStones(stones));
+  }
+}
