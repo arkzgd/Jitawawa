435015a638290d8110353626dafeb4ee508bb4bb
Hui ZHANG <arkzgd@gmail.com>
2021-07-01 23:25:18 -0400

    Leetcode #947 Most Stone Removed With Same Row Or Column
    Naively wrong.

--- src/com/ihaveaname/java/leetcode/graph/MostStoneRemovedWithSameRowOrColumn_947.java
+++ src/com/ihaveaname/java/leetcode/graph/MostStoneRemovedWithSameRowOrColumn_947.java
@@ -1,10 +1,46 @@
 package com.ihaveaname.java.leetcode.graph;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MostStoneRemovedWithSameRowOrColumn_947 {
   class Solution {
+    class Node {
+      int i;
+      int j;
+      Node parent;
+
+      Node(int i, int j) {
+        this.i = i;
+        this.j = j;
+        this.parent = null;
+      }
+
+      boolean connected(Node another) {
+        return this.i == another.i || this.j == another.j;
+      }
+    }
+
     public int removeStones(int[][] stones) {
-      int n = stones.length;
-      return 0;
+      int N = stones.length;
+      Set<Node> incorporated = new HashSet<>();
+
+      for (int[] s : stones) {
+        Node stone = new Node(s[0], s[1]);
+        for (Node n : incorporated) {
+          if (n.connected(stone)) {
+            stone.parent = n;
+          }
+        }
+        incorporated.add(stone);
+      }
+
+      int count = 0;
+      for (Node n : incorporated) {
+        if (n.parent == null) count++;
+      }
+
+      return N - count;
     }
   }
 
@@ -13,6 +49,9 @@ public class MostStoneRemovedWithSameRowOrColumn_947 {
         new MostStoneRemovedWithSameRowOrColumn_947();
     Solution solution = mostStoneRemovedWithSameRowOrColumn_947.new Solution();
     int[][] stones = new int[][] {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
+    System.out.println(solution.removeStones(stones));
+
+    stones = new int[][] {{0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}};
     System.out.println(solution.removeStones(stones));
   }
 }
