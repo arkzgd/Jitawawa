d9fbd01b3c305f8907bc243c7a736f29cc28c392
Hui ZHANG <arkzgd@gmail.com>
2021-07-03 13:41:34 -0400

    Leetcode #947 Most Stone Removed With Same Row Or Column
    155 ms.

--- src/com/ihaveaname/java/leetcode/graph/MostStoneRemovedWithSameRowOrColumn_947.java
+++ src/com/ihaveaname/java/leetcode/graph/MostStoneRemovedWithSameRowOrColumn_947.java
@@ -29,13 +29,9 @@ public class MostStoneRemovedWithSameRowOrColumn_947 {
         Node stone = new Node(s[0], s[1]);
         for (Node n : incorporated) {
           if (n.connected(stone)) {
-            if (n.j == stone.j) {
-              if (n.i < stone.i) n.parent = stone;
-              else stone.parent = n;
-            } else if (n.i == stone.i) {
-              if (n.j < stone.j) n.parent = stone;
-              else stone.parent = n;
-            }
+            Node p = n;
+            while (p.parent != null) p = p.parent;
+            if (p != stone) p.parent = stone;
           }
         }
         incorporated.add(stone);
@@ -64,6 +60,9 @@ public class MostStoneRemovedWithSameRowOrColumn_947 {
     System.out.println(solution.removeStones(stones));
 
     stones = new int[][] {{0, 1}, {1, 2}, {1, 3}, {3, 3}, {2, 3}, {0, 2}};
+    System.out.println(solution.removeStones(stones));
+
+    stones = new int[][] {{0, 1}, {0, 2}, {4, 3}, {2, 4}, {0, 3}, {1, 1}};
     System.out.println(solution.removeStones(stones));
   }
 }
