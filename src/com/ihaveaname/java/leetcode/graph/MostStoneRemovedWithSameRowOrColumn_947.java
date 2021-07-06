7f16bc68489d9c421dfc79170223ea9a90d6af1d
Hui ZHANG <arkzgd@gmail.com>
2021-07-05 21:46:58 -0400

    Revert "Leetcode #947 Most Stone Removed With Same Row Or Column"
    This reverts commit 3d8c22d3

--- src/com/ihaveaname/java/leetcode/graph/MostStoneRemovedWithSameRowOrColumn_947.java
+++ src/com/ihaveaname/java/leetcode/graph/MostStoneRemovedWithSameRowOrColumn_947.java
@@ -21,11 +21,13 @@ public class MostStoneRemovedWithSameRowOrColumn_947 {
       }
     }
 
-    private void findParent(Node v, final Node newParent) {
-      if (v.parent != null && v.parent != newParent) {
-        findParent(v.parent, newParent);
-      }
-      v.parent = newParent;
+    private Node findParent(Node v) {
+      if (v == null || v.parent == null) return v;
+
+      Node p = findParent(v.parent);
+      v.parent = p;
+
+      return p;
     }
 
     public int removeStones(int[][] stones) {
@@ -36,7 +38,8 @@ public class MostStoneRemovedWithSameRowOrColumn_947 {
         Node stone = new Node(s[0], s[1]);
         for (Node n : incorporated) {
           if (n.connected(stone)) {
-            findParent(n, stone);
+            Node p = findParent(n);
+            if (p != stone) p.parent = stone;
           }
         }
         incorporated.add(stone);
