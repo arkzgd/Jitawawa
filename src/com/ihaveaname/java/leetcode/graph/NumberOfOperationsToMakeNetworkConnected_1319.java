400ba604a69f08d160a27930981df23247c69e91
Hui ZHANG <arkzgd@gmail.com>
2021-07-08 23:56:11 -0400

    Leetcode #1319 Number Of Operations To Make Network Connected
    Naive.

--- src/com/ihaveaname/java/leetcode/graph/NumberOfOperationsToMakeNetworkConnected_1319.java
+++ src/com/ihaveaname/java/leetcode/graph/NumberOfOperationsToMakeNetworkConnected_1319.java
@@ -1,9 +1,27 @@
 package com.ihaveaname.java.leetcode.graph;
 
+import java.util.Arrays;
+
 public class NumberOfOperationsToMakeNetworkConnected_1319 {
   class Solution {
     public int makeConnected(int n, int[][] connections) {
-      return 0;
+      int[] parent = new int[n];
+      Arrays.fill(parent, -1);
+      if (connections.length >= n - 1) {
+        for (int[] connection : connections) {
+          int i = connection[0];
+          int j = connection[0];
+          int p = i;
+          while (parent[p] != -1) p = parent[p];
+          if (p != i) {
+            parent[i] = p;
+            parent[j] = p;
+          } else parent[j] = i;
+        }
+        int count = 0;
+        for (int p : parent) if (p == -1) count++;
+        return count - 1;
+      } else return -1;
     }
   }
 
@@ -12,7 +30,7 @@ public class NumberOfOperationsToMakeNetworkConnected_1319 {
         new NumberOfOperationsToMakeNetworkConnected_1319();
     Solution solution = numberOfOperationsToMakeNetworkConnected_1319.new Solution();
 
-    int n = 6;
+    int n = 4;
     int[][] connected = new int[][] {{0, 1}, {0, 2}, {1, 2}};
     System.out.println(solution.makeConnected(n, connected));
   }
